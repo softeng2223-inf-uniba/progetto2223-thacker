7e8b52a0f5be9ed91bdcf550ed4010c0a1f8efe6
shokk47 <gamerk789@gmail.com>
2023-05-13 18:58:11 +0200

    Aggiunto alcuni commenti al codice

--- src/main/java/controllers/CommandHandler.java
+++ src/main/java/controllers/CommandHandler.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class CommandHandler {
-    private static final String MSG_INVALID         = "Comando non valido";
+    private static final String MSG_INVALID = "Comando non valido";
 
     public static String readCommand() {
         BufferedReader buffer = null;
@@ -22,13 +22,13 @@ public class CommandHandler {
     }
 
     public static boolean executeCommand(final String input) {
-        String[] tokens = input.split(" ");
-        String comand   = tokens[0];
+        String[] tokens = input.split(" ");     // contiene il comando e i parametri
+        String comand   = tokens[0];                  // contiene il comando
 
         switch (comand) {
             case "/prova":
                 if (needParams(tokens, 2)) {
-                    // esegue comando
+                    // codice per eseguire comando "/prova"
                     return true;
                 }
                 break;
@@ -40,15 +40,15 @@ public class CommandHandler {
     }
 
     private static boolean needParams(final String[] tokens, final int params) {
-        if (tokens.length != params + 1) {
+        if (tokens.length != params + 1) {  // +1 perchè tokens[0] è il comando
             return false;
         }
         return true;
     }
 
     private static boolean needParams(final String[] tokens, final int min, final int max) {
-        int minNumOfTokens = min + 1;
-        int maxNumOfTokens = max + 1;
+        int minNumOfTokens = min + 1; // +1 perchè tokens[0] è il comando
+        int maxNumOfTokens = max + 1; // +1 perchè tokens[0] è il comando
         if (minNumOfTokens < maxNumOfTokens) {
             int length = tokens.length;
             if (length <= maxNumOfTokens && length >= minNumOfTokens) {
