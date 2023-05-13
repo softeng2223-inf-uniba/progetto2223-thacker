b415eec095bfa490141b0b3f182d4cf630fc3c0b
shokk47 <gamerk789@gmail.com>
2023-05-13 19:22:48 +0200

    Ho creato i javadoc per executeCommand e needParams

--- src/main/java/controllers/CommandHandler.java
+++ src/main/java/controllers/CommandHandler.java
@@ -22,6 +22,12 @@ public final class CommandHandler {
         return command;
     }
 
+    /**
+     * La funzione esegue il comando passato come parametro.
+     * In base al comando esegue un'azione diversa.
+     * @param input comando da eseguire
+     * @return true se il comando è stato eseguito correttamente, false altrimenti
+     */
     public static boolean executeCommand(final String input) {
         String[] tokens = input.split(" ");     // contiene il comando e i parametri
         String comand   = tokens[0];                  // contiene il comando
@@ -40,6 +46,14 @@ public final class CommandHandler {
         return false;
     }
 
+    /**
+     * La funzione controlla se il comando presente in tokens
+     * contiene params parametri.
+     * @param tokens comando da eseguire con i parametri
+     * @param params numero di parametri che il comando deve avere
+     * @return true se il comando ha params parametri, false altrimenti
+     */
+
     private static boolean needParams(final String[] tokens, final int params) {
         if (tokens.length != params + 1) {  // +1 perchè tokens[0] è il comando
             return false;
@@ -47,6 +61,14 @@ public final class CommandHandler {
         return true;
     }
 
+    /**
+     * La funzione controlla se il comando presente in tokens
+     * contiene un numero di parametri compreso tra min e max.
+     * @param tokens comando da eseguire con i parametri
+     * @param min numero minimo di parametri che il comando deve avere
+     * @param max numero massimo di parametri che il comando deve avere
+     * @return true se il comando ha un numero di parametri compreso tra min e max, false altrimenti
+     */
     private static boolean needParams(final String[] tokens, final int min, final int max) {
         int minNumOfTokens = min + 1; // +1 perchè tokens[0] è il comando
         int maxNumOfTokens = max + 1; // +1 perchè tokens[0] è il comando
