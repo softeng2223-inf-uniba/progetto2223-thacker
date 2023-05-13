ae9dbe303d7585cac048e417225dc7f366059852
anto31ad <anto31.ad@gmail.com>
2023-05-13 18:28:25 +0200

    Rimossi trailing spaces segnalati da CheckStyle

--- src/main/java/controllers/CommandHandler.java
+++ src/main/java/controllers/CommandHandler.java
@@ -7,11 +7,11 @@ import java.io.IOException;
 public class CommandHandler {
     private static final String MSG_INVALID         = "Comando non valido";
     private static final String MSG_INIT            = "Inserisci comando: ";
-	
+
     public static String readCommand() {
         BufferedReader buffer = null;
         String command = "";
-        
+
         try{
             buffer = new BufferedReader(new InputStreamReader(System.in));
             command = buffer.readLine();
@@ -21,6 +21,7 @@ public class CommandHandler {
 
         return command;
     }
+
     public static boolean executeCommand(String input){
         String[] tokens = input.split(" ");
         String comand   = tokens[0];
@@ -36,13 +37,15 @@ public class CommandHandler {
         System.out.println(MSG_INVALID);
         return false;
     }
-	private static boolean needParams(String[] tokens, int params){
+
+    private static boolean needParams(String[] tokens, int params){
         if(tokens.length != params+1){
             return false;
         }
         return true;
-    }    
-	private static boolean needParams(String[] tokens, int params, int minNumOfTokens, int maxNumOfTokens){
+    }
+
+    private static boolean needParams(String[] tokens, int params, int minNumOfTokens, int maxNumOfTokens){
         params += 1;
         if(minNumOfTokens<maxNumOfTokens){
             int length = tokens.length;
