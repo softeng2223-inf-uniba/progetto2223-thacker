931318dd2aaec743392fa00c0b6f2423a6e84c45
anto31ad <anto31.ad@gmail.com>
2023-05-14 15:53:23 +0200

    Creata la classe DifficultyManager e aggiunti membri di base

---
+++ src/main/java/it/uniba/app/game/DifficultyManager.java
@@ -0,0 +1,19 @@
+package it.uniba.app.game;
+
+// TODO javadoc
+public class DifficultyManager {
+   
+    /* CONSTANTS */
+    private static final String EASY_NAME = "Facile";
+    private static final String MED_NAME = "Medio";
+    private static final String HARD_NAME = "Difficile";
+
+    private static final int EASY_MAX_ATTEMPTS = 50;
+    private static final int MED_MAX_ATTEMPTS  = 30;
+    private static final int HARD_MAX_ATTEMPTS = 10;
+    
+    /* VARIABLES */
+    private static String curLevelName;
+    private static int maxFailable;
+
+}
