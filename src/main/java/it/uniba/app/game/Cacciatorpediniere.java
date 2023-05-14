7d29a3c31c95ca10a4975e8b03cc0e010737f533
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-05-14 20:02:12 +0200

    Aggiunte le costanti SHIP_SIZE e NUMBER_OF_INSTANCES

--- src/main/java/it/uniba/app/game/Cacciatorpediniere.java
+++ src/main/java/it/uniba/app/game/Cacciatorpediniere.java
@@ -2,5 +2,9 @@ package it.uniba.app.game;
 
 public class Cacciatorpediniere extends Ship {
 
+    private static final int SHIP_SIZE = 2;
+
+    private static final int NUMBER_OF_INSTANCES = 4;
+
 }
 
