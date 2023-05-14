1cc4e84bc9d25f37f2e466afa394b790f6082b8c
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-05-14 21:12:28 +0200

    Aggiunti attributi final e name a Portaerei

--- src/main/java/it/uniba/app/game/Portaerei.java
+++ src/main/java/it/uniba/app/game/Portaerei.java
@@ -2,5 +2,14 @@ package it.uniba.app.game;
 
 public class Portaerei extends Ship {
 
+    private static final int SHIP_SIZE = 5;
+
+    private static final int NUMBER_OF_INSTANCES = 1;
+
+    private static final String PORTAEREI_COLOR = "\u001B[33m";
+
+    private String name;
+
+
 }
 
