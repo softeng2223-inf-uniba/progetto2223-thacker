2b2616f1940caf84eed1d367015bd4840b8974e2
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-05-14 21:00:53 +0200

    Aggiunti attributi final e attributo name a Corazzata

--- src/main/java/it/uniba/app/game/Corazzata.java
+++ src/main/java/it/uniba/app/game/Corazzata.java
@@ -2,5 +2,14 @@ package it.uniba.app.game;
 
 
 public class Corazzata extends Ship {
+
+    private static final int SHIP_SIZE = 4;
+
+    private static final int NUMBER_OF_INSTANCES = 2;
+
+    private static final String CORAZZATA_COLOR = "\u001B[32m";
+
+    private String name;
+
  
 }
