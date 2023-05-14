7d4b70d817fa4de8a80f2fd68f2e36d6aa124750
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-05-14 20:04:02 +0200

    Aggiunto attributo final CACCIATORPEDINIERE_COLOR

--- src/main/java/it/uniba/app/game/Cacciatorpediniere.java
+++ src/main/java/it/uniba/app/game/Cacciatorpediniere.java
@@ -6,5 +6,7 @@ public class Cacciatorpediniere extends Ship {
 
     private static final int NUMBER_OF_INSTANCES = 4;
 
+    private static final String CACCIATORPEDINIERE_COLOR = "\u001B[31m";
+
 }
 
