03e0e531eced9750a42277dd97c42c5a10401d9c
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-05-14 18:35:23 +0200

    Aggiunti attributi relativi al simbolo delle navi

--- src/main/java/it/uniba/app/game/Ship.java
+++ src/main/java/it/uniba/app/game/Ship.java
@@ -3,4 +3,10 @@ package it.uniba.app.game;
 public abstract class Ship {
 
     private static final int SHIPS_NUMBER = 4;
+
+    private static final String SHIPS_SYMBOL = "\u22A0";
+
+    private static final String SYMBOL_WHITE_COLOR = "\u001B[0m";
+
+
 }
