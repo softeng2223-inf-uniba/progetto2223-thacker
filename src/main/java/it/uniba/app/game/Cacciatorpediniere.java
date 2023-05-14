6ae5ad997235d8d20c2da7200b20c2e3d516603c
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-05-14 20:19:38 +0200

    Aggiunti JavaDoc agli attributi final

--- src/main/java/it/uniba/app/game/Cacciatorpediniere.java
+++ src/main/java/it/uniba/app/game/Cacciatorpediniere.java
@@ -1,11 +1,22 @@
 package it.uniba.app.game;
 
 public class Cacciatorpediniere extends Ship {
-
+    /**
+     * {@code SHIP_SIZE} rappresenta la lunghezza
+     * della nave Cacciatorpediniere.
+     */
     private static final int SHIP_SIZE = 2;
-
+    /**
+     * {@code NUMBER_OF_INSTANCES} rappresenta il numero di navi
+     * {@code Cacciatorpediniere} che si possono istanziare
+     * durante una partita.
+    */
     private static final int NUMBER_OF_INSTANCES = 4;
-
+    /**
+     * {@code CACCIATORPEDINIERE_COLOR} contiene il colore scelto
+     * per la nave Corazzata che servirà per rappresentare
+     * la nave all'interno della {@code Griglia delle Navi}.
+     */
     private static final String CACCIATORPEDINIERE_COLOR = "\u001B[31m";
 
     private String name;
