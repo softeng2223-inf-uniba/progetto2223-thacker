3c8422fd539864e3f788f6d2613390fb755a779b
anto31ad <anto31.ad@gmail.com>
2023-05-14 16:12:32 +0200

    Rimossi whitespace segnalati da CheckStyle

--- src/main/java/it/uniba/app/game/DifficultyManager.java
+++ src/main/java/it/uniba/app/game/DifficultyManager.java
@@ -2,7 +2,7 @@ package it.uniba.app.game;
 
 // TODO javadoc
 public class DifficultyManager {
-   
+
     /* CONSTANTS */
     private static final String EASY_NAME = "Facile";
     private static final String MED_NAME = "Medio";
@@ -11,13 +11,13 @@ public class DifficultyManager {
     private static final int EASY_MAX_ATTEMPTS = 50;
     private static final int MED_MAX_ATTEMPTS  = 30;
     private static final int HARD_MAX_ATTEMPTS = 10;
-    
+
     /* VARIABLES */
     private static String curLevelName;
     private static int maxFailable;
 
     /* METHODS */
-    
+
     DifficultyManager(){
         setEasyLevel();
     }
@@ -44,5 +44,5 @@ public class DifficultyManager {
     public static int getMaxFailedAttempts(){
         return maxFailable;
     }
-    
+
 }
