0f81a21a6024bd1dcf9637fc8c9012f453df5d18
anto31ad <anto31.ad@gmail.com>
2023-05-14 16:47:53 +0200

    Aggiunto Javadoc in DifficultyManager

--- src/main/java/it/uniba/app/game/DifficultyManager.java
+++ src/main/java/it/uniba/app/game/DifficultyManager.java
@@ -1,6 +1,18 @@
 package it.uniba.app.game;
 
-// TODO javadoc
+/**
+ * Modella il gestore della difficoltà che condiziona la singola partita.
+ *
+ * Attualmente i livelli impostabili sono tre: <i>Easy, Medium, Hard</i>;
+ * Il livello predefinito è <i>Easy</i>.
+ * Un livello può essere impostato invocando il rispettivo metodo:
+ *
+ * <pre>
+ * setEasyLevel();   //imposta Easy
+ * setMedLevel();    //imposta Medium
+ * setHardLevel();   //imposta Hard
+ * </pre>
+ */
 public final class DifficultyManager {
 
     /* CONSTANTS */
@@ -22,25 +34,45 @@ public final class DifficultyManager {
         setEasyLevel();
     }
 
+    /**
+     * Imposta il livello Easy (Facile).
+    */
     public static void setEasyLevel() {
         curLevelName = EASY_NAME;
         maxFailable = EASY_MAX_ATTEMPTS;
     }
 
+    /**
+     * Imposta il livello Medium (Medio).
+     */
     public static void setMedLevel() {
         curLevelName = MED_NAME;
         maxFailable = MED_MAX_ATTEMPTS;
     }
 
+    /**
+     * Imposta il livello Hard (Difficile).
+     */
     public static void setHardLevel() {
         curLevelName = HARD_NAME;
         maxFailable = HARD_MAX_ATTEMPTS;
     }
 
+    /**
+     * Fornisce il nome del livello di difficoltà impostato correntemente.
+     *
+     * @return nome del livello corrente.
+     */
     public static String getLevelName() {
         return curLevelName;
     }
 
+    /**
+     * Fornisce il numero massimo di tentativi falliti,
+     * basato sul livello di difficoltà corrente.
+     *
+     * @return numero massimo di tentativi falliti.
+     */
     public static int getMaxFailedAttempts() {
         return maxFailable;
     }
