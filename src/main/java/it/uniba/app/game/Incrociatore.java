afc0bac7b324a773f1f64d91e9ff400e6b36a46b
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-05-14 20:42:23 +0200

    Aggiunto attributi final a Incrociatore

--- src/main/java/it/uniba/app/game/Incrociatore.java
+++ src/main/java/it/uniba/app/game/Incrociatore.java
@@ -2,4 +2,10 @@ package it.uniba.app.game;
 
 public class Incrociatore extends Ship {
 
+    private static final int SHIP_SIZE = 3;
+
+    private static final int NUMBER_OF_INSTANCES = 3;
+
+    private static final String INCROCIATORE_COLOR = "\u001B[34m";
+
 }
