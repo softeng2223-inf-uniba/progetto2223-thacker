4c2d0fdd2e16c59ccb7be5b30f0ac2e8cd55ba1a
FredrickHZO <federico.armagno97@gmail.com>
2023-05-14 13:09:19 +0200

    Creato scheletro classe Coordinate
    Co-Authored-By: Giovanni Cirigliano <36540365+shokk47@users.noreply.github.com>

---
+++ src/main/java/it/uniba/app/utility/Coordinate.java
@@ -0,0 +1,10 @@
+package it.uniba.app.utility;
+
+/**
+ * JavaDoc momentaneo. La classe {@code Coordinate} astrae
+ * il concetto di riga e colonna per la {code Board}.
+ */
+public class Coordinate {
+    private int row;
+    private int col;
+}
