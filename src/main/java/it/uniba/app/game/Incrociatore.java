adeb7b4c3196e2a70dff9626e3d3355ab485b9fb
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-05-15 15:43:52 +0200

    Aggiunti JavaDoc attributi final Incrociatore

--- src/main/java/it/uniba/app/game/Incrociatore.java
+++ src/main/java/it/uniba/app/game/Incrociatore.java
@@ -1,11 +1,22 @@
 package it.uniba.app.game;
 
 public class Incrociatore extends Ship {
-
+    /**
+     * {@code SHIP_SIZE} rappresenta la lunghezza
+     * della nave {@code Incrociatore}.
+     */
     private static final int SHIP_SIZE = 3;
-
+    /**
+     * {@code NUMBER_OF_INSTANCES} rappresenta il numero di navi
+     * {@code Incrociatore} che si possono istanziare
+     * durante una partita.
+    */
     private static final int NUMBER_OF_INSTANCES = 3;
-
+    /**
+     * {@code INCROCIATORE_COLOR} contiene il colore scelto
+     * per la nave Corazzata che servirà per rappresentare
+     * la nave all'interno della {@code Griglia delle Navi}.
+     */
     private static final String INCROCIATORE_COLOR = "\u001B[34m";
 
     private String name;
