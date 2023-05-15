6fc0034b6d3128205894c67f530cca10afae5740
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-05-15 15:53:03 +0200

    Aggiunti JavaDoc agli attributi final di Portaerei

--- src/main/java/it/uniba/app/game/Portaerei.java
+++ src/main/java/it/uniba/app/game/Portaerei.java
@@ -1,11 +1,22 @@
 package it.uniba.app.game;
 
 public class Portaerei extends Ship {
-
+    /**
+     * {@code SHIP_SIZE} rappresenta la lunghezza
+     * della nave {@code Portaerei}.
+     */
     private static final int SHIP_SIZE = 5;
-
+    /**
+     * {@code NUMBER_OF_INSTANCES} rappresenta il numero di navi
+     * {@code Portaerei} che si possono istanziare
+     * durante una partita.
+    */
     private static final int NUMBER_OF_INSTANCES = 1;
-
+    /**
+     * {@code PORTAEREI_COLOR} contiene il colore scelto
+     * per la nave Corazzata che servirà per rappresentare
+     * la nave all'interno della {@code Griglia delle Navi}.
+     */
     private static final String PORTAEREI_COLOR = "\u001B[33m";
 
     private String name;
