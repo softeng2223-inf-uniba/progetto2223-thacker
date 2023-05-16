6afad1678544b26c9df26383d88728ce0cd1680a
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-16 15:17:15 +0200

    Aggiunti altri attributi nella classe Board

--- src/main/java/it/uniba/app/game/Board.java
+++ src/main/java/it/uniba/app/game/Board.java
@@ -4,8 +4,10 @@ package it.uniba.app.game;
  * JavaDoc momentaneo.
  */
 public class Board {
+    private static final int NUM_OF_CELL = 10;
     /**
      * JavaDoc momentaneo.
      */
     private Ship[][] shipMap;
+    private String[][] playerMap;
 }
