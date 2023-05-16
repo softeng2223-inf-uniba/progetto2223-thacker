06f5813fc5c9f372ead7d8a27a887f357d7018aa
Giuseppe <g.durso15@studenti.uniba.it>
2023-05-16 11:04:01 +0200

    Risolto secondo errore CheckStyle
    Ho risolto questo errore:
    Error: eckstyle] [ERROR] /home/runner/work/progetto2223-thacker/progetto2223-thacker/src/main/java/it/uniba/app/utility/PrintHandler.java:6:23: Parameter msg should be final. [FinalParameters]

--- src/main/java/it/uniba/app/utility/PrintHandler.java
+++ src/main/java/it/uniba/app/utility/PrintHandler.java
@@ -3,7 +3,7 @@ package it.uniba.app.utility;
 public class PrintHandler {
     private static final String PRINTEXAMPLE = "PROVA";
 
-    public void print(String msg) {
+    public void print(final String msg) {
         System.out.println(msg);
     }
 }
