7667dff47824995669d6c41c0b4b9ab6dbdb2903
Giuseppe <g.durso15@studenti.uniba.it>
2023-05-16 10:48:32 +0200

    Creato metodo print
    Questo metodo stampa una stringa

--- src/main/java/it/uniba/app/utility/PrintHandler.java
+++ src/main/java/it/uniba/app/utility/PrintHandler.java
@@ -2,4 +2,8 @@ package it.uniba.app.utility;
 
 public class PrintHandler {
     private static final PRINTEXAMPLE = "PROVA";
+
+    public void print(String msg){
+        System.out.println(msg);
+    }
 }
