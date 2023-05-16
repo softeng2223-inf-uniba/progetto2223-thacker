4b178a53b120e7eeb6006438171c190ec10eabdd
Giuseppe <g.durso15@studenti.uniba.it>
2023-05-16 11:06:50 +0200

    Inserito javadoc di print

--- src/main/java/it/uniba/app/utility/PrintHandler.java
+++ src/main/java/it/uniba/app/utility/PrintHandler.java
@@ -3,6 +3,10 @@ package it.uniba.app.utility;
 public class PrintHandler {
     private static final String PRINTEXAMPLE = "PROVA";
 
+    /**
+     * Stampa il messaggio fornito in input sulla console.
+     * @param msg messaggio da stampare
+     */
     public void print(final String msg) {
         System.out.println(msg);
     }
