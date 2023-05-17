4130fea270fb94f3f2342cb3b2c8a51f58bbe753
Vsb23 <v.birardi3@studenti.uniba.it>
2023-05-17 12:25:18 +0200

    Re-implementata la classe Help

--- src/main/java/it/uniba/app/utility/help/Help.java
+++ src/main/java/it/uniba/app/utility/help/Help.java
@@ -1,104 +1,33 @@
 package it.uniba.app.utility.help;
 
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 /**
  * <p>
  * Classe principale. Contiene le funzioni che stampa a video il file di testo
  * contenente le istruzioni per l'utilizzo del programma.
  */
 public final class Help {
-    /**
-     * Variabile costante che indica il tempo di attesa tra la stampa di una stringa del titolo e la successiva,
-     * in millisecondi.
-     */
-    private static final int THREAD_DELAY_TITLE = 100;
-    /**
-     * Variabile costante che indica il tempo di attesa tra la stampa di un carattere e l'altro, in millisecondi.
-     *
-     */
-    private static final int THREAD_DELAY_TEXT = 5;
+    public static final int SLEEP_TIME = 1;
+    private static final String  TITLE =
+      "   ██████╗ ███████╗ ██████╗  ██████╗ ██╗     ███████╗    ██████╗ ██╗    ██████╗  █████╗ ███████╗███████╗" + "\n"
+    + "   ██╔══██╗██╔════╝██╔════╝ ██╔═══██╗██║     ██╔════╝    ██╔══██╗██║    ██╔══██╗██╔══██╗██╔════╝██╔════╝" + "\n"
+    + "   ██████╔╝█████╗  ██║  ███╗██║   ██║██║     █████╗      ██║  ██║██║    ██████╔╝███████║███████╗█████╗" + "\n"
+    + "   ██╔══██╗██╔══╝  ██║   ██║██║   ██║██║     ██╔══╝      ██║  ██║██║    ██╔══██╗██╔══██║╚════██║██╔══╝" + "\n"
+    + "   ██║  ██║███████╗╚██████╔╝╚██████╔╝███████╗███████╗    ██████╔╝██║    ██████╔╝██║  ██║███████║███████╗" + "\n"
+    + "   ╚═╝  ╚═╝╚══════╝ ╚═════╝  ╚═════╝ ╚══════╝╚══════╝    ╚═════╝ ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝" + "\n";
 
+    private Help() { };
     /**
-    * Variabile che contiene il file title.txt.
+     * <p>.
+    *
     */
-    private static final File TITLE_FILE = new File("src/main/java/it/uniba/app/utility/help/title.txt");
-
-    /**
-    * Variabile che contiene il file text.txt.
-    */
-    private static final File TEXT_FILE = new File("src/main/java/it/uniba/app/utility/help/text.txt");
-
-    /**
-     * <p>
-     * Costruttore privato di Help.
-     * </p>
-     * Ha zero argomenti.Dichiarato privato al fine di evitare l'istanziazione della classe
-     * (che è una classe di sola utilità e non ha bisogno di essere istanziata).
-     */
-    private Help() { }
-    /**
-     * <p>
-     * Legge il file title.txt e stampa a video ogni stringa da cui è composto quest'ultimo.
-     * </p>
-     * Tra la stampa di una stringa  e l'altra c'è un delay di 100 millisecondi.
-     *
-     */
-    private static void printTitle() throws IOException {
-        BufferedReader reader = null;
-        String title = "";
-        try {
-        reader = new BufferedReader(new FileReader(TITLE_FILE));
-            while (reader.read() != -1) {
-                title = reader.readLine();
-                Thread.sleep(THREAD_DELAY_TITLE);
-                title = title + "\r";
-                System.out.println(title);
+    public static void printText() {
+        for (int i = 0; i < TITLE.length(); i++) {
+            System.out.print(TITLE.charAt(i));
+            try {
+                Thread.sleep(SLEEP_TIME);
+            } catch (InterruptedException err) {
+                System.out.println("Errore: " + err);
             }
-            title = title + "\r";
-        } catch (IOException e) {
-            System.out.println("Input/Output error" + e);
-        } catch (InterruptedException e) {
-            System.out.println("InterruptedException error: " + e);
-        }
-        reader.close();
-    }
-    /**
-     * <p>
-     *
-     * Legge il file text.txt e stampa a video ogni carattere da cui è composto quest'ultimo.
-     *</p>
-     * Tra la stampa di un carattere e l'altro c'è un delay di 5 millisecondi.
-     */
-    private static void printText() throws IOException {
-        BufferedReader readerText = null;
-        try {
-            readerText = new BufferedReader(new FileReader(TEXT_FILE));
-            int num;
-            while ((num = readerText.read()) != -1) {
-                System.out.print((char) num);
-                Thread.sleep(THREAD_DELAY_TEXT);
-            }
-        } catch (IOException e) {
-                System.out.println("Input/Output error: " + e);
-        } catch (InterruptedException p) {
-            System.out.println("Thread interrotto" + p);
-        }
-        readerText.close();
-    }
-    /**
-     * <p>
-     * Invoca le uniche due funzioni di Help: printTitle e printText.
-     *
-     */
-    public static void printHelp() {
-        try {
-            Help.printTitle();
-            Help.printText();
-        } catch (IOException e) {
-            System.out.println("Input/Output error: " + e);
         }
     }
 }
