b7f8c752c3bf75d14c8e5a0dbe5fbf303094d3d3
FredrickHZO <federico.armagno97@gmail.com>
2023-05-17 11:23:20 +0200

    Creta base della classe InitializeGame

---
+++ src/main/java/it/uniba/app/game/InitializeGame.java
@@ -0,0 +1,17 @@
+package it.uniba.app.game;
+
+
+/**
+ * JavaDoc momentaneo.
+ */
+public class InitializeGame {
+    private Board map;
+
+    /**
+     * JavaDoc momentaneo costruttore.
+     * @param gameMap mappa da inizializzare.
+     */
+    public InitializeGame(final Board gameMap) {
+        map = gameMap;
+    }
+}
