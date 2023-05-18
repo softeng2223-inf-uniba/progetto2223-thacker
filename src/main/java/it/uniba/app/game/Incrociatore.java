36d4184fea90ac9fce63e8531e7fc0778336c6d1
FredrickHZO <federico.armagno97@gmail.com>
2023-05-18 15:30:29 +0200

    Reso pubblico getShipColoredSymbol Incrociatore

--- src/main/java/it/uniba/app/game/Incrociatore.java
+++ src/main/java/it/uniba/app/game/Incrociatore.java
@@ -44,7 +44,7 @@ public class Incrociatore extends Ship {
      * colore del tipo di nave specifico.
      * @return stringa che contiene il colore della nave.
      */
-    String getShipColoredSymbol() {
+    public String getShipColoredSymbol() {
         return INCROCIATORE_COLOR;
     }
     /**
