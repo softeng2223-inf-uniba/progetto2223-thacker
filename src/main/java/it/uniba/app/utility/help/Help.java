854e20eab836d403b0aebb4d54c1e25ee2e3896e
Vsb23 <v.birardi3@studenti.uniba.it>
2023-05-18 11:50:22 +0200

    Aggiornato attributo TEXT
    Ho apportato un aggiornamento all'attributo per adattarlo ai requisiti della tua issue. In particolare, ho reso la descrizione più concisa.

--- src/main/java/it/uniba/app/utility/help/Help.java
+++ src/main/java/it/uniba/app/utility/help/Help.java
@@ -15,28 +15,22 @@ public final class Help {
     + "   ╚═╝  ╚═╝╚══════╝ ╚═════╝  ╚═════╝ ╚══════╝╚══════╝    ╚═════╝ ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝"
     + "\n\n";
 
+    /**
+     * {@code TEXT} contiene una descrizione concisa
+     * seguita dalla lista di comandi disponibili.
+     */
     private static final String TEXT =
-
-    "Il giocatore avrà a disposizione due griglie di dimensione 10x10,"
-    + "una dove saranno disposte le navi in maniera casuale, e l'altra dove potrà"
-    + "indicare le coordinate della casella che ha intenzione di colpire." + "\n\n"
-    + "Prima di cominciare la partita, il giocatore dovrà scegliere il livello di difficoltà di quest'ultima." + "\n\n"
-    + "L'impostazione della difficoltà potrà essere effettuata tramite i comandi" + "\n\n"
-    + "- /facile," + "\n"
-    + "- /medio," + "\n"
-    + "- /difficile." + "\n\n"
-    + "Il livello facile prevede un numero massimo di tentativi"
-    + "fallibili pari a 50, il livello medio 30 e il livello difficile 10." + "\n\n"
-    + "Per mostrare il livello di difficoltà durante la partita, il giocatore deve "
-    + "digitare il comando /mostralivello.Una volta impostata la difficoltà, il giocatore potrà "
-    + "cominciare una nuova partita digitando il comando /gioca." + "\n\n"
-    + "Con il comando /mostranavi, il giocatore visualizzerà l'elenco delle navi "
-    + "a disposizione, in particolare: il nome, la dimensione in quadratini e "
-    + "il numero di esemplari istanziabili." + "\n\n"
-    + "Nel caso si riscontrassero delle difficoltà nel colpire le navi dell'avversario, "
-    + "il giocatore può invocare il comando /svelagriglia per scoprire  la griglia dell'avversario, "
-    + "ovvero la griglia con le navi posizionate." + "\n\n"
-    + "" + "\n";
+    "Benvenuto in BattleShip!\n\n"
+    + "Scopo del gioco: affondare tutte le navi prima di aver terminato il numero di tentativi fallibili.\n"
+    + "Per giocare, utilizzare i seguenti comandi:\n\n"
+    + "/gioca \n"
+    + "/facile \n"
+    + "/medio \n" 
+    + "/difficile \n"
+    + "/mostralivello \n"
+    + "/svelagriglia \n"
+    + "/mostranavi \n"
+    + "/esci";
 
     private Help() { };
 
