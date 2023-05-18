814adb8eea67964357baaaa9aca47cb7c0885de7
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-05-18 13:05:40 +0200

    Creata classe PrinthShips in utility con JavaDoc

---
+++ src/main/java/it/uniba/app/utility/PrintShips.java
@@ -0,0 +1,13 @@
+package it.uniba.app.utility;
+/**
+ * Classe necessaria per eseguire il comando
+ * {@code /mostranavi} e ottenere l'output desiderato.
+ * Permette di stampare a video, di ogni tipo
+ * di nave, il nome, la dimensione in quadrati e il numero
+ * di esemplari da affondare. Utilizza i metodi
+ * implementati in ciascuna sottoclasse di {@code Ship}
+ * relativi alla stampa delle informazioni di ciascuna
+ * specializzazione di nave.
+*/
+public final class PrintShips {
+}
