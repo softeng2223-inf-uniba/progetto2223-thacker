ebdf8e4b59dc0dd8a1f6452f1f9dc625d6a39741
FredrickHZO <federico.armagno97@gmail.com>
2023-05-18 15:29:37 +0200

    Reso pubblico metodo getShipColoredSymbol

--- src/main/java/it/uniba/app/game/Portaerei.java
+++ src/main/java/it/uniba/app/game/Portaerei.java
@@ -44,7 +44,7 @@ public class Portaerei extends Ship {
      * colore del tipo di nave specifico.
      * @return stringa che contiene il colore della nave.
      */
-    String getShipColoredSymbol() {
+    public String getShipColoredSymbol() {
         return PORTAEREI_COLOR;
     }
     /**
