e9e0ed0d9db1118682fdf5862f9c0f07e4dd6eab
shokk47 <gamerk789@gmail.com>
2023-05-19 15:50:32 +0200

    Modificato execute di MostraLivello

--- src/main/java/it/uniba/app/utility/commands/noargs/MostraLivello.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/MostraLivello.java
@@ -1,8 +1,13 @@
 package it.uniba.app.utility.commands.noargs;
 import it.uniba.app.utility.commands.noargs.NoArgs;
+import it.uniba.app.game.DifficultyManager;
+import it.uniba.app.utility.PrintHandler;
 
 public class MostraLivello implements NoArgs {
     public void execute(){
-        System.out.println("mostralivello");
+        PrintHandler.print("Il livello di difficoltà selezionato è: ");
+        PrintHandler.println(DifficultyManager.getLevelName());
+        PrintHandler.print("Il numero massimo di tentativi fallibili è: ");
+        PrintHandler.println(String.valueOf(DifficultyManager.getMaxFailedAttempts()));
     }
 }
