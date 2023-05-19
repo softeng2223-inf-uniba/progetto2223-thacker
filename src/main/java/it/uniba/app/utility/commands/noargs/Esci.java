2f19d68f6af42a93d216d7ff7db23c590c6539ad
shokk47 <gamerk789@gmail.com>
2023-05-19 17:06:26 +0200

    Corretto import Esci

--- src/main/java/it/uniba/app/utility/commands/noargs/Esci.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/Esci.java
@@ -1,6 +1,6 @@
 package it.uniba.app.utility.commands.noargs;
 import it.uniba.app.utility.commands.CommandHandler;
-import it.uniba.app.utility.commands.noargs.NoArg;
+import it.uniba.app.utility.commands.noargs.NoArgs;
 import it.uniba.app.utility.PrintHandler;
 /**
  * Classe del comando /esci.
