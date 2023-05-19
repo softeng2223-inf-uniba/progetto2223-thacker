f9954dd4de1af5d86a54655558ab8594d0f9bff3
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 14:54:19 +0200

    Inserito javadoc di execute

--- src/main/java/it/uniba/app/utility/commands/FlagHandler.java
+++ src/main/java/it/uniba/app/utility/commands/FlagHandler.java
@@ -11,6 +11,14 @@ public class FlagHandler {
         flags.put("--help", new HelpCommand());
         flags.put("-h", new HelpCommand());
     }
+    /**
+     *  Esegue il comando corrispondente alla flag inserito.
+     *  I comandi flag non hanno argomenti, quindi passare un array di stringhe
+     *  con una dimensione maggiore di 1 provocherà la stampa di un errore. 
+     *  Se il flag è riconosciuto, viene eseguito il comando corrispondente.
+     *  
+     *  @param args array di stringhe contenente il flag inserito
+     */
     public static void execute(final String[] args){
         if(args.length == 1){
             NoArgs flagInput = flags.get(args[0]);
