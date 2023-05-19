3eb691b67c58a3815843f61b98ef40eccc42e528
shokk47 <gamerk789@gmail.com>
2023-05-19 12:16:47 +0200

    Aggiunto flag "--help" in flags

--- src/main/java/it/uniba/app/utility/commands/FlagHandler.java
+++ src/main/java/it/uniba/app/utility/commands/FlagHandler.java
@@ -6,4 +6,7 @@ import java.util.Map;
 public class FlagHandler {
     private static final String MSG_FLAG_NOT_RECOGNIZED = "Flag non riconosciuto";
     private static final Map<String, NoArgs> flags = new HashMap<>();
+    static{
+        flags.put("--help", new HelpCommand());
+    }
 }
