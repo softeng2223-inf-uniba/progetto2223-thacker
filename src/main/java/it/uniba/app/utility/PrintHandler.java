ee625e1b955a7372313f43ca44804ebd7f9e3bd0
FredrickHZO <federico.armagno97@gmail.com>
2023-05-19 11:21:29 +0200

    Inserita funzione di stampa in PrintHandler

--- src/main/java/it/uniba/app/utility/PrintHandler.java
+++ src/main/java/it/uniba/app/utility/PrintHandler.java
@@ -1,5 +1,8 @@
 package it.uniba.app.utility;
+
 import it.uniba.app.game.DifficultyManager;
+import it.uniba.app.game.Board;
+
 
 /**
  * La classe PrintHandler fornisce funzionalità
@@ -21,6 +24,19 @@ public final class PrintHandler {
     public static void println(final String msg) {
         System.out.println(msg);
     }
+
+    public static void printPlayerMap(final Board map) {
+        System.out.print("    A    B    C    D    E    F    G    H    I    L\n\n");
+        for (int i = 0; i < Board.getSize(); i++) {
+            System.out.print(Integer.toString(i) + ":  ");
+            for (int j = 0; j < Board.getSize(); j++) {
+                System.out.print(map.getFromPlayerMap(
+                    new Coordinate(i, j)) + "    ");
+            }
+            System.out.print("\n\n");
+        }
+    }
+
     /**
      * Stampa il livello di difficoltà corrente.
      */
