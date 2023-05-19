ebd7942e92d77509ab54062eb97228ce8321a990
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 15:18:11 +0200

    Creata classe Facile

---
+++ src/main/java/it/uniba/app/utility/commands/noargs/Facile.java
@@ -0,0 +1,8 @@
+package it.uniba.app.utility.commands.noargs;
+import it.uniba.app.utility.commands.noargs.NoArgs;
+
+public class Facile implements NoArgs {
+    public void execute(){
+        System.out.println("facile");
+    }
+}
