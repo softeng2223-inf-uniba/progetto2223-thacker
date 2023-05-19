cdedad1966c22aa1bd49497fc557c06fea981103
shokk47 <gamerk789@gmail.com>
2023-05-19 15:52:46 +0200

    Modificato execute di Gioca

--- src/main/java/it/uniba/app/utility/commands/noargs/Gioca.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/Gioca.java
@@ -1,7 +1,10 @@
 package it.uniba.app.utility.commands.noargs;
+import it.uniba.app.game.Board;
+import it.uniba.app.game.InitializeGame;
 import it.uniba.app.utility.commands.noargs.NoArgs;
 public class Gioca implements NoArgs {
     public void execute() {
-        System.out.println("Gioca");
+        InitializeGame init = new InitializeGame();
+        init.initGame(new Board());
     }
 }
