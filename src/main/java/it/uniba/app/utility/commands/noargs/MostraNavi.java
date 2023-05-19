027f4a472ebd66f74aaedc39a0fbf8beead0ce5b
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 16:06:34 +0200

    Inserito javadoc execute MostraNavi

--- src/main/java/it/uniba/app/utility/commands/noargs/MostraNavi.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/MostraNavi.java
@@ -3,6 +3,10 @@ import it.uniba.app.utility.PrintShips;
 import it.uniba.app.utility.commands.noargs.NoArgs;
 
 public class MostraNavi implements NoArgs {
+    /**
+     *  Il metodo stampa le navi da affondare
+     *  durante una classica partita.
+     */
     public void execute(){
         PrintShips.showShips();
     }
