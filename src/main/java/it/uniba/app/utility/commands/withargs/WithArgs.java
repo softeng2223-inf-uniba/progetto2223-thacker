14e79f32b5c11926eb3e3adc3e80e0a6baf8b54e
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 14:56:59 +0200

    Javadoc di execute WithArgs

--- src/main/java/it/uniba/app/utility/commands/withargs/WithArgs.java
+++ src/main/java/it/uniba/app/utility/commands/withargs/WithArgs.java
@@ -1,5 +1,11 @@
 package it.uniba.app.utility.commands.withargs;
 
 public interface WithArgs {
+    /**
+     *  Il metodo esegue il codice del comand
+     *  con parametri selezionato..
+     * 
+     *  @param args array di stringhe contenente i parametri inseriti
+     */
     void execute(String[] args);
 }
