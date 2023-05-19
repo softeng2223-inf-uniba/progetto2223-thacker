e2aa989f2dddfbf0f61f18cdf51728f876242db5
anto31ad <anto31.ad@gmail.com>
2023-05-19 19:03:36 +0200

    Aggiornata execute() in SvelaGriglia.java

--- src/main/java/it/uniba/app/utility/commands/noargs/SvelaGriglia.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/SvelaGriglia.java
@@ -1,4 +1,9 @@
 package it.uniba.app.utility.commands.noargs;
+
+import it.uniba.app.game.BoardManager;
+import it.uniba.app.game.Ship;
+import it.uniba.app.utility.Coordinate;
+
 /**
  * Classe del comando /svelagriglia.
  * Contiene il codice da eseguire con il comando /svelagriglia.
@@ -11,6 +16,27 @@ public class SvelaGriglia implements NoArgs {
      *  colpita verrà mostrato lo stato.
      */
     public void execute() {
-        System.out.println("svelagrliglia");
+        printGameMap();
+    }
+
+        /**
+     *  La funzione stampa la mappa di gioco con le nave posizionati.
+     *  Nel caso in cui non sia posizionata nave in una determinata coordinata
+     *  viene stampato il simbolo "~".
+     */
+    private static void printGameMap() {
+        System.out.print("    A    B    C    D    E    F    G    H    I    L\n\n");
+        for (int i = 0; i < BoardManager.getDim(); i++) {
+            System.out.print(i + ":  ");
+            for (int j = 0; j < BoardManager.getDim(); j++) {
+                Ship item = BoardManager.access().getElement(new Coordinate(i, j));
+                if (item == null) {
+                    System.out.print("~    ");
+                } else {
+                    System.out.print(item + "    ");
+                }
+            }
+            System.out.print("\n\n");
+        }
     }
 }
