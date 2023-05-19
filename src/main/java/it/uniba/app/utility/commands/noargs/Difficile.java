840e925984db9c785497fe47d9cec99899f0b234
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 16:28:17 +0200

    Aggiunto javadoc Difficile

--- src/main/java/it/uniba/app/utility/commands/noargs/Difficile.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/Difficile.java
@@ -2,7 +2,10 @@ package it.uniba.app.utility.commands.noargs;
 import it.uniba.app.game.DifficultyManager;
 import it.uniba.app.utility.PrintHandler;
 import it.uniba.app.utility.commands.noargs.NoArgs;
-
+/**
+ * Classe del comando /difficile.
+ * Contiene il codice da eseguire con il comando /difficile.
+ */
 public class Difficile implements NoArgs {
     /*
      *  Il metodo imposta il livello di difficoltà 
