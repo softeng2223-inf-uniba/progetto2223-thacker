cc8ccb99a51a249bc79484ca3f4e8706e0b323d4
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 16:06:04 +0200

    Inserito javadoc execute MostraLivello

--- src/main/java/it/uniba/app/utility/commands/noargs/MostraLivello.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/MostraLivello.java
@@ -4,6 +4,10 @@ import it.uniba.app.game.DifficultyManager;
 import it.uniba.app.utility.PrintHandler;
 
 public class MostraLivello implements NoArgs {
+    /**
+     *  Il metodo stampa il livello di difficoltà corrente
+     *  e il numero massimo di tentativi fallibili.
+     */
     public void execute(){
         PrintHandler.print("Il livello di difficoltà selezionato è: ");
         PrintHandler.println(DifficultyManager.getLevelName());
