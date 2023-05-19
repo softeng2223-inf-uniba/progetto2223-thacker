48fef5c0577bbe715713c7a571b6b83a237472cd
Vsb23 <v.birardi3@studenti.uniba.it>
2023-05-19 17:19:35 +0200

    Risolti errori checkstyle in Medio

--- src/main/java/it/uniba/app/utility/commands/noargs/Medio.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/Medio.java
@@ -1,17 +1,16 @@
 package it.uniba.app.utility.commands.noargs;
 import it.uniba.app.game.DifficultyManager;
 import it.uniba.app.utility.PrintHandler;
-import it.uniba.app.utility.commands.noargs.NoArgs;
 /**
  * Classe del comando /medio.
  * Contiene il codice da eseguire con il comando /medio.
  */
 public class Medio implements NoArgs {
-    /*
-     *  Il metodo imposta il livello di difficoltà 
+    /**
+     *  Il metodo imposta il livello di difficoltà
      *  del gioco a medio e stampa il nome del livello.
      */
-    public void execute(){
+    public void execute() {
         DifficultyManager.setMedLevel();
         PrintHandler.printLevelName();
     }
