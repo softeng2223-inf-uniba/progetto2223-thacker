5819268a557a9977ff2961d7b4cc30ac037b7f81
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 16:09:56 +0200

    Inserito javadoc execute Esci

--- src/main/java/it/uniba/app/utility/commands/noargs/Esci.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/Esci.java
@@ -2,6 +2,11 @@ package it.uniba.app.utility.commands.noargs;
 import it.uniba.app.utility.commands.noargs.NoArgs;
 
 public class Esci implements NoArgs {
+    /**
+     *  Il metodo permette di uscire in modo
+     *  sicuro dal gioco stampando un messaggio 
+     *  di conferma.
+     */
     public void execute(){
         System.out.println("Esci");
     }
