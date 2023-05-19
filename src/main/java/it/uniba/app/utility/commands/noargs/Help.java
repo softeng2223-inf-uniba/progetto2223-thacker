eac75feccd92f037d989a3f49bdd9ec0072ea67e
shokk47 <gamerk789@gmail.com>
2023-05-19 15:37:59 +0200

    Aggiunto attributo TEXT
    TEXT è una stringa contente tutti una breve descrizione del gioco e una lista di comandi disponibili

--- src/main/java/it/uniba/app/utility/commands/noargs/Help.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/Help.java
@@ -7,6 +7,18 @@ public class Help implements NoArgs {
      */
 
     private static final int TEXT_DELAY = 100;
+    private static final String TEXT =
+            "Benvenuto in BattleShip!\n\n"
+                    + "Scopo del gioco: affondare tutte le navi prima di aver terminato il numero di tentativi fallibili.\n"
+                    + "Per giocare, utilizzare i seguenti comandi:\n\n"
+                    + "/gioca \n"
+                    + "/facile \n"
+                    + "/medio \n"
+                    + "/difficile \n"
+                    + "/mostralivello \n"
+                    + "/svelagriglia \n"
+                    + "/mostranavi \n"
+                    + "/esci";
 
     public void execute(){
         System.out.println("help");
