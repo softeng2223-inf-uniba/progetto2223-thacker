31d9ce925be8afa5aa9f5cddf1806da6bdba4469
shokk47 <gamerk789@gmail.com>
2023-05-19 12:15:26 +0200

    Aggiunto attributo MSG_FLAG_NOT_RECOGNIZED

--- src/main/java/it/uniba/app/utility/commands/FlagHandler.java
+++ src/main/java/it/uniba/app/utility/commands/FlagHandler.java
@@ -4,4 +4,5 @@ import it.uniba.app.utility.commands.noargs.*;
 import java.util.HashMap;
 import java.util.Map;
 public class FlagHandler {
+    private static final String MSG_FLAG_NOT_RECOGNIZED = "Flag non riconosciuto";
 }
