d78cdfc347cbf43df8bb81cd1785191c42b28320
Vsb23 <v.birardi3@studenti.uniba.it>
2023-05-19 17:20:17 +0200

    Eliminato import ridondante in MostraLivello

--- src/main/java/it/uniba/app/utility/commands/noargs/MostraLivello.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/MostraLivello.java
@@ -1,5 +1,4 @@
 package it.uniba.app.utility.commands.noargs;
-import it.uniba.app.utility.commands.noargs.NoArgs;
 import it.uniba.app.game.DifficultyManager;
 import it.uniba.app.utility.PrintHandler;
 /**
@@ -11,7 +10,7 @@ public class MostraLivello implements NoArgs {
      *  Il metodo stampa il livello di difficoltà corrente
      *  e il numero massimo di tentativi fallibili.
      */
-    public void execute(){
+    public void execute() {
         PrintHandler.print("Il livello di difficoltà selezionato è: ");
         PrintHandler.println(DifficultyManager.getLevelName());
         PrintHandler.print("Il numero massimo di tentativi fallibili è: ");
