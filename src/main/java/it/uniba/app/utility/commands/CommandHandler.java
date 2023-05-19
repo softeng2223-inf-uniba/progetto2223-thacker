64f9405795d8fa48923184a6ce4e0fb507770285
shokk47 <gamerk789@gmail.com>
2023-05-19 11:39:50 +0200

    Aggiunto attributo ENCODER_USED

--- src/main/java/it/uniba/app/utility/commands/CommandHandler.java
+++ src/main/java/it/uniba/app/utility/commands/CommandHandler.java
@@ -3,4 +3,5 @@ package it.uniba.app.utility.commands;
 import it.uniba.app.utility.commands.withargs.*;
 import it.uniba.app.utility.commands.noargs.*;
 public class CommandHandler {
+    private static final String ENCODER_USED = "UTF-8";
 }
