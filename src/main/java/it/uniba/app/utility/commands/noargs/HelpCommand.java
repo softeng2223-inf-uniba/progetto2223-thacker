065776e4d6243825a63a08e8cef10d40fc724133
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 14:55:18 +0200

    Javadoc di execute HelpCommand

--- src/main/java/it/uniba/app/utility/commands/noargs/HelpCommand.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/HelpCommand.java
@@ -1,6 +1,10 @@
 package it.uniba.app.utility.commands.noargs;
 import it.uniba.app.utility.commands.noargs.NoArgs;
 public class HelpCommand implements NoArgs {
+    /*
+     *  Il comando help stampa un messaggio con una breve
+     *  descrizione del gioco e la lista dei comandi disponbili.
+     */
     public void execute(){
         System.out.println("help");
     }
