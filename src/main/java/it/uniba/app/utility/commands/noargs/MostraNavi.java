32e588a86e2f568b80fee3f8a838484eebc3c1a6
shokk47 <gamerk789@gmail.com>
2023-05-19 15:49:16 +0200

    Modificato execute di MostraNavi

--- src/main/java/it/uniba/app/utility/commands/noargs/MostraNavi.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/MostraNavi.java
@@ -1,8 +1,9 @@
 package it.uniba.app.utility.commands.noargs;
+import it.uniba.app.utility.PrintShips;
 import it.uniba.app.utility.commands.noargs.NoArgs;
 
 public class MostraNavi implements NoArgs {
     public void execute(){
-        System.out.println("mostranavi");
+        PrintShips.showShips();
     }
 }
