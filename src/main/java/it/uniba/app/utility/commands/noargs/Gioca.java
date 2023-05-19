ecb15a9ca06d905502e4890a43b18a55f058fc2f
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 16:30:17 +0200

    Aggiunto javadoc Gioca

--- src/main/java/it/uniba/app/utility/commands/noargs/Gioca.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/Gioca.java
@@ -2,6 +2,10 @@ package it.uniba.app.utility.commands.noargs;
 import it.uniba.app.game.Board;
 import it.uniba.app.game.InitializeGame;
 import it.uniba.app.utility.commands.noargs.NoArgs;
+/**
+ * Classe del comando /gioca.
+ * Contiene il codice da eseguire con il comando /gioca.
+ */
 public class Gioca implements NoArgs {
     /**
      *  Il metodo inizializza una nuova partita.
