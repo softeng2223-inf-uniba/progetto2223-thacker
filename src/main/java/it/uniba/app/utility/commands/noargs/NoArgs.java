1a55d6ba36ba3e8b7ba9ebfaea195c2a9d729a15
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 14:56:12 +0200

    Javadoc di execute NoArgs

--- src/main/java/it/uniba/app/utility/commands/noargs/NoArgs.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/NoArgs.java
@@ -1,5 +1,8 @@
 package it.uniba.app.utility.commands.noargs;
-
+ */
 public interface NoArgs {
+    /**
+     *  Il metodo esegue il codice del comando selezionato.
+     */
     void execute();
 }
