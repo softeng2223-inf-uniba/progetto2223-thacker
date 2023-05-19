3634621a25162947cccf9fe1688efe5125dfe7c4
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 16:31:46 +0200

    Aggiunto javadoc NoArgs

--- src/main/java/it/uniba/app/utility/commands/noargs/NoArgs.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/NoArgs.java
@@ -1,5 +1,7 @@
 package it.uniba.app.utility.commands.noargs;
-
+/**
+ * Interfaccia pubblica dei comandi senza parametri
+ */
 public interface NoArgs {
     /**
      *  Il metodo esegue il codice del comando selezionato.
