74ebea9599e71659cb9e6881b419c3cfee13d020
Vsb23 <v.birardi3@studenti.uniba.it>
2023-05-19 17:15:18 +0200

    Risolti errori vari

--- src/main/java/it/uniba/app/utility/commands/noargs/Difficile.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/Difficile.java
@@ -1,17 +1,16 @@
 package it.uniba.app.utility.commands.noargs;
 import it.uniba.app.game.DifficultyManager;
 import it.uniba.app.utility.PrintHandler;
-import it.uniba.app.utility.commands.noargs.NoArgs;
 /**
  * Classe del comando /difficile.
  * Contiene il codice da eseguire con il comando /difficile.
  */
 public class Difficile implements NoArgs {
-    /*
-     *  Il metodo imposta il livello di difficoltà 
+    /**
+     *  Il metodo imposta il livello di difficoltà
      *  del gioco a difficile e stampa il nome del livello.
      */
-    public void execute(){
+    public void execute() {
         DifficultyManager.setHardLevel();
         PrintHandler.printLevelName();
     }
