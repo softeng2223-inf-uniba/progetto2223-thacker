fefa2238362eef04854a61df8c4afb65ca7bec75
FredrickHZO <federico.armagno97@gmail.com>
2023-05-19 11:23:54 +0200

    Aggiunto JavaDoc a printPlayerMap

--- src/main/java/it/uniba/app/utility/PrintHandler.java
+++ src/main/java/it/uniba/app/utility/PrintHandler.java
@@ -25,12 +25,16 @@ public final class PrintHandler {
         System.out.println(msg);
     }
 
-    public static void printPlayerMap(final Board map) {
+    /**
+     * Stampa la mappa dei colpi che usa il giocatore.
+     * @param board board contenente le mappe colpi e navi.
+     */
+    public static void printPlayerMap(final Board board) {
         System.out.print("    A    B    C    D    E    F    G    H    I    L\n\n");
         for (int i = 0; i < Board.getSize(); i++) {
             System.out.print(Integer.toString(i) + ":  ");
             for (int j = 0; j < Board.getSize(); j++) {
-                System.out.print(map.getFromPlayerMap(
+                System.out.print(board.getFromPlayerMap(
                     new Coordinate(i, j)) + "    ");
             }
             System.out.print("\n\n");
