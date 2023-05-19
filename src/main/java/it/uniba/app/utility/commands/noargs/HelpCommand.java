73d8f80fbf1478ccb6ff7a565c00450dccf54cf7
shokk47 <gamerk789@gmail.com>
2023-05-19 11:50:02 +0200

    Aggiunto import
    Aggiunto import dell'interfaccia NoArgs in HelpCommand

--- src/main/java/it/uniba/app/utility/commands/noargs/HelpCommand.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/HelpCommand.java
@@ -1,5 +1,5 @@
 package it.uniba.app.utility.commands.noargs;
-
+import it.uniba.app.utility.commands.noargs.NoArgs;
 public class HelpCommand implements NoArgs {
     public void execute(){
         System.out.println("help");
