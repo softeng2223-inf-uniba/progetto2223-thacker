48f8da446dcea1f08e51b14a41f339fcbd4833fc
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 15:21:11 +0200

    Creata classe MostraLivello

---
+++ src/main/java/it/uniba/app/utility/commands/noargs/MostraLivello.java
@@ -0,0 +1,8 @@
+package it.uniba.app.utility.commands.noargs;
+import it.uniba.app.utility.commands.noargs.NoArgs;
+
+public class MostraLivello implements NoArgs {
+    public void execute(){
+        System.out.println("mostralivello");
+    }
+}
