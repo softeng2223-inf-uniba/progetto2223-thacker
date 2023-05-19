d38fc2376aa812559d90b7ee3768a36787145be3
shokk47 <gamerk789@gmail.com>
2023-05-19 15:47:03 +0200

    Modificato execute di Difficile

--- src/main/java/it/uniba/app/utility/commands/noargs/Difficile.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/Difficile.java
@@ -1,8 +1,11 @@
 package it.uniba.app.utility.commands.noargs;
+import it.uniba.app.game.DifficultyManager;
+import it.uniba.app.utility.PrintHandler;
 import it.uniba.app.utility.commands.noargs.NoArgs;
 
 public class Difficile implements NoArgs {
     public void execute(){
-        System.out.println("difficile");
+        DifficultyManager.setHardLevel();
+        PrintHandler.printLevelName();
     }
 }
