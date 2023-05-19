fb745a1d6a1163bbeb4c71e7c8122e2ecf3b0182
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 16:07:13 +0200

    Inserito javadoc execute SvelaGriglia

--- src/main/java/it/uniba/app/utility/commands/noargs/SvelaGriglia.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/SvelaGriglia.java
@@ -2,6 +2,12 @@ package it.uniba.app.utility.commands.noargs;
 import it.uniba.app.utility.commands.noargs.NoArgs;
 
 public class SvelaGriglia implements NoArgs {
+    /**
+     *  Il metodo stampa la griglia di gioco
+     *  con tutte le navi posizionate.
+     *  Nel caso in cui una nave risulta già
+     *  colpita verrà mostrato lo stato.
+     */
     public void execute(){
         System.out.println("svelagrliglia");
     }
