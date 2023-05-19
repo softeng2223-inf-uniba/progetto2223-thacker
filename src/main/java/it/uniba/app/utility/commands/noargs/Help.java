1d3078609b72c85ea69b4c65a718065754b94bff
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-05-19 14:58:58 +0200

    Modificato nome classe Help

--- src/main/java/it/uniba/app/utility/commands/noargs/Help.java
+++ src/main/java/it/uniba/app/utility/commands/noargs/Help.java
@@ -1,6 +1,6 @@
 package it.uniba.app.utility.commands.noargs;
 import it.uniba.app.utility.commands.noargs.NoArgs;
-public class HelpCommand implements NoArgs {
+public class Help implements NoArgs {
     /*
      *  Il comando help stampa un messaggio con una breve
      *  descrizione del gioco e la lista dei comandi disponbili.
