b81413deb063ce747f2a695ecb92b12fb50e5ab8
Vsb23 <v.birardi3@studenti.uniba.it>
2023-05-19 17:36:39 +0200

    Corretti segnalazioni checkbugs in FlagHandler

--- src/main/java/it/uniba/app/utility/commands/FlagHandler.java
+++ src/main/java/it/uniba/app/utility/commands/FlagHandler.java
@@ -2,6 +2,7 @@ package it.uniba.app.utility.commands;
 
 
 import it.uniba.app.utility.commands.noargs.Help;
+import it.uniba.app.utility.commands.noargs.NoArgs;
 import java.util.HashMap;
 import java.util.Map;
 import it.uniba.app.utility.PrintHandler;
@@ -10,29 +11,34 @@ import it.uniba.app.utility.PrintHandler;
  *  i flag inseriti dall'utente. Per esempio i flag --help e -h stampano
  *  un messaggio con i comandi disponibili.
  */
-public class FlagHandler {
+public final class FlagHandler {
     private static final String MSG_FLAG_NOT_RECOGNIZED = "Flag non riconosciuto";
-    private static final Map<String, NoArgs> flags = new HashMap<>();
-    static{
-        flags.put("--help", new Help());
-        flags.put("-h", new Help());
+    private static final Map<String, NoArgs> FLAGS = new HashMap<>();
+    static {
+        FLAGS.put("--help", new Help());
+        FLAGS.put("-h", new Help());
+    }
+    private FlagHandler() {
+
     }
     /**
      *  Esegue il comando corrispondente alla flag inserito.
      *  I comandi flag non hanno argomenti, quindi passare un array di stringhe
-     *  con una dimensione maggiore di 1 provocherà la stampa di un errore. 
+     *  con una dimensione maggiore di 1 provocherà la stampa di un errore.
      *  Se il flag è riconosciuto, viene eseguito il comando corrispondente.
-     *  
+     *
      *  @param args array di stringhe contenente il flag inserito
      */
-    public static void execute(final String[] args){
-        if(args.length == 1){
-            NoArgs flagInput = flags.get(args[0]);
-            if(flagInput != null){
+    public static void execute(final String[] args) {
+        if (args.length == 1) {
+            NoArgs flagInput = FLAGS.get(args[0]);
+            if (flagInput != null) {
                 flagInput.execute();
-            }else{
+            } else {
                 PrintHandler.println(MSG_FLAG_NOT_RECOGNIZED);
             }
-        }else PrintHandler.println(MSG_FLAG_NOT_RECOGNIZED);
+        } else {
+            PrintHandler.println(MSG_FLAG_NOT_RECOGNIZED);
+        }
     }
 }
