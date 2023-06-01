7584c8e21ef8d4ddc29bf9d1eee396f22007e78c
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-06-01 10:02:32 +0200

    Creata Ship.java in ship con prime costanti

---
+++ src/main/java/it/uniba/app/battleship/ship/Ship.java
@@ -0,0 +1,8 @@
+package it.uniba.app.battleship.ship;
+
+public final class Ship implements Cloneable {
+
+    private static final String SYMBOL        = "\u22A0";
+    private static final String DEFAULT_COLOR = "\u001B[0m";
+}
+
