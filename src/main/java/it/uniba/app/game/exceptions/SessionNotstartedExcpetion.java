2e2e77bb0ad379d69f557cf908b179d2132176b9
Vsb23 <v.birardi3@studenti.uniba.it>
2023-06-01 12:05:56 +0200

    Implementata SessionNotStartedException

---
+++ src/main/java/it/uniba/app/game/exceptions/SessionNotstartedExcpetion.java
@@ -0,0 +1,12 @@
+package it.uniba.app.game.exceptions;
+
+/** TODO replace. */
+public class SessionNotStartedException extends GameException {
+
+    /**
+    * TODO javadoc.
+    */
+    public SessionNotStartedException() {
+        super("La sessione non è ancora iniziata.");
+    }
+}
