22125ce1a1eddef4d278338ea123f282ccd29bd2
Giuseppe <g.durso15@studenti.uniba.it>
2023-06-01 12:03:50 +0200

    Aggiunto costruttore privato Input

--- src/main/java/it/uniba/app/utility/Input.java
+++ src/main/java/it/uniba/app/utility/Input.java
@@ -1,5 +1,7 @@
 package it.uniba.app.utility;
 
 public class Input {
-    public static final String CHARSET_NAME = "UTF-8"; 
+    public static final String CHARSET_NAME = "UTF-8";
+    
+    private Input() { }
 }
