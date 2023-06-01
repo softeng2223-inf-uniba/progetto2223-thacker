d060430c026851d5eb2fe2fb3bb979e2fcd58ea6
Vsb23 <v.birardi3@studenti.uniba.it>
2023-06-01 11:52:47 +0200

    Implementata GameExceptions

---
+++ src/main/java/it/uniba/app/game/exceptions/GameException.java
@@ -0,0 +1,14 @@
+package it.uniba.app.game.exceptions;
+
+/**
+ * Modella una categoria di eccezioni legate al gioco (battleship).
+ */
+public class GameException extends Exception {
+
+    /**
+     * TODO javadoc.
+     */
+    public GameException(final String message) {
+        super(message);
+    }
+}
