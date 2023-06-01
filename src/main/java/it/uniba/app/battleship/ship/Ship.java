335c96e420f73568e0bba16ab1e0f549748c5193
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-06-01 10:11:52 +0200

    Aggiunte costanti numero istanze per tipo di navi
    Queste costanti contengono il numero massimo di esemplari per tipo di nave che si possono istanziare in una partita.

--- src/main/java/it/uniba/app/battleship/ship/Ship.java
+++ src/main/java/it/uniba/app/battleship/ship/Ship.java
@@ -9,5 +9,10 @@ public final class Ship implements Cloneable {
     private static final String CRUISER_STR    = "Incrociatore";
     private static final String BATTLESHIP_STR = "Corazzata";
     private static final String FLATTOP_STR    = "Portaerei";
+
+    private static final int DESTROYER_INSTANCES  = 4;
+    private static final int CRUISER_INSTANCES    = 3;
+    private static final int BATTLESHIP_INSTANCES = 2;
+    private static final int FLATTOP_INSTANCES    = 1;
 }
 
