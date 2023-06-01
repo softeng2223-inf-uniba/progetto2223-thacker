63577dd46371f638d0394b3e2eed4e1bf96c76e1
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-06-01 10:19:14 +0200

    Modificati nomi costanti

--- src/main/java/it/uniba/app/battleship/ship/Ship.java
+++ src/main/java/it/uniba/app/battleship/ship/Ship.java
@@ -5,19 +5,19 @@ public final class Ship implements Cloneable {
     private static final String SYMBOL        = "\u22A0";
     private static final String DEFAULT_COLOR = "\u001B[0m";
 
-    private static final String DESTROYER_STR  = "Cacciatorpediniere";
-    private static final String CRUISER_STR    = "Incrociatore";
-    private static final String BATTLESHIP_STR = "Corazzata";
-    private static final String FLATTOP_STR    = "Portaerei";
+    private static final String CACCIATORPEDINIERE_STR  = "Cacciatorpediniere";
+    private static final String INCROCIATORE_STR    = "Incrociatore";
+    private static final String CORAZZATA_STR = "Corazzata";
+    private static final String PORTAEREI_STR    = "Portaerei";
 
-    private static final int DESTROYER_INSTANCES  = 4;
-    private static final int CRUISER_INSTANCES    = 3;
-    private static final int BATTLESHIP_INSTANCES = 2;
-    private static final int FLATTOP_INSTANCES    = 1;
+    private static final int CACCIATORPEDINIERE_INSTANCES  = 4;
+    private static final int INCROCIATORE_INSTANCES    = 3;
+    private static final int CORAZZATA_INSTANCES = 2;
+    private static final int PORTAEREI_INSTANCES    = 1;
 
-    private static final int DESTROYER_SIZE  = 2;
-    private static final int CRUISER_SIZE    = 3;
-    private static final int BATTLESHIP_SIZE = 4;
-    private static final int FLATTOP_SIZE    = 5;
+    private static final int CACCIATORPEDINIERE_SIZE  = 2;
+    private static final int INCROCIATORE_SIZE    = 3;
+    private static final int CORAZZATA_SIZE = 4;
+    private static final int PORTAEREI_SIZE    = 5;
 }
 
