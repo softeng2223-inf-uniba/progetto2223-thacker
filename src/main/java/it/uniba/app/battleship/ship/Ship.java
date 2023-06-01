84d4c0ba7af40b5a20f06c7131508eab72b72302
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-06-01 10:05:42 +0200

    Aggiunte costanti con i nomi dei tipi di nave

--- src/main/java/it/uniba/app/battleship/ship/Ship.java
+++ src/main/java/it/uniba/app/battleship/ship/Ship.java
@@ -4,5 +4,10 @@ public final class Ship implements Cloneable {
 
     private static final String SYMBOL        = "\u22A0";
     private static final String DEFAULT_COLOR = "\u001B[0m";
+
+    private static final String DESTROYER_STR  = "Cacciatorpediniere";
+    private static final String CRUISER_STR    = "Incrociatore";
+    private static final String BATTLESHIP_STR = "Corazzata";
+    private static final String FLATTOP_STR    = "Portaerei";
 }
 
