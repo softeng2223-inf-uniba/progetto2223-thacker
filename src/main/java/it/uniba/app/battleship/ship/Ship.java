533bea3083fe8d744163527fb9d27f77d6fafd62
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-06-01 15:18:41 +0200

    Ristrutturazione del codice

--- src/main/java/it/uniba/app/battleship/ship/Ship.java
+++ src/main/java/it/uniba/app/battleship/ship/Ship.java
@@ -12,25 +12,25 @@ public final class Ship implements Cloneable {
     private static final String SYMBOL        = "\u22A0";
     private static final String DEFAULT_COLOR = "\u001B[0m";
 
-    private static final String CACCIATORPEDINIERE_STR  = "Cacciatorpediniere";
-    private static final String INCROCIATORE_STR    = "Incrociatore";
-    private static final String CORAZZATA_STR = "Corazzata";
-    private static final String PORTAEREI_STR    = "Portaerei";
+    private static final String CACCIATORPEDINIERE_STR = "Cacciatorpediniere";
+    private static final String INCROCIATORE_STR       = "Incrociatore";
+    private static final String CORAZZATA_STR          = "Corazzata";
+    private static final String PORTAEREI_STR          = "Portaerei";
 
-    private static final int CACCIATORPEDINIERE_INSTANCES  = 4;
-    private static final int INCROCIATORE_INSTANCES    = 3;
-    private static final int CORAZZATA_INSTANCES = 2;
-    private static final int PORTAEREI_INSTANCES    = 1;
+    private static final int CACCIATORPEDINIERE_INSTANCES = 4;
+    private static final int INCROCIATORE_INSTANCES       = 3;
+    private static final int CORAZZATA_INSTANCES          = 2;
+    private static final int PORTAEREI_INSTANCES          = 1;
 
-    private static final int CACCIATORPEDINIERE_SIZE  = 2;
-    private static final int INCROCIATORE_SIZE    = 3;
-    private static final int CORAZZATA_SIZE = 4;
-    private static final int PORTAEREI_SIZE    = 5;
+    private static final int CACCIATORPEDINIERE_SIZE = 2;
+    private static final int INCROCIATORE_SIZE       = 3;
+    private static final int CORAZZATA_SIZE          = 4;
+    private static final int PORTAEREI_SIZE          = 5;
 
-    private static final String CACCIATORPEDINIERE_COLOR  = "\u001B[31m";
-    private static final String INCROCIATORE_COLOR    = "\u001B[34m";
-    private static final String CORAZZATA_COLOR = "\u001B[32m";
-    private static final String PORTAEREI_COLOR    = "\u001B[33m";
+    private static final String CACCIATORPEDINIERE_COLOR = "\u001B[31m";
+    private static final String INCROCIATORE_COLOR       = "\u001B[34m";
+    private static final String CORAZZATA_COLOR          = "\u001B[32m";
+    private static final String PORTAEREI_COLOR          = "\u001B[33m";
 
     private int typeId;
     private int hitsTaken;
@@ -115,6 +115,7 @@ public final class Ship implements Cloneable {
     public static String resetColor() {
         return DEFAULT_COLOR;
     }
+
 
     /* === INSTANCE METHODS === */
 
@@ -203,10 +204,7 @@ public final class Ship implements Cloneable {
 
         try {
             clone = (Ship) super.clone();
-            System.out.println("Ship Cloned!"); // TODO remove. debug purposes
-        } catch (CloneNotSupportedException e) {
-            // TODO something
-        }
+        } catch (CloneNotSupportedException e) { }
 
         return clone;
     }
