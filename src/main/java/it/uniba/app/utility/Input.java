614b8781b824dd9d12f974398d131dfc90e04366
Giuseppe <g.durso15@studenti.uniba.it>
2023-06-05 14:48:05 +0200

    Aggiunto import FileInputStream

--- src/main/java/it/uniba/app/utility/Input.java
+++ src/main/java/it/uniba/app/utility/Input.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.Properties;
+import java.io.FileInputStream;
 
 /**
  * La classe gestisce l'input da parte dell'utente.
