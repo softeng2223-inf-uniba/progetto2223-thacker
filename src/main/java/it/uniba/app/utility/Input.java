a2bef645fe1236c2fe13dd169cd26ec901eb9832
Giuseppe <g.durso15@studenti.uniba.it>
2023-06-05 14:47:10 +0200

    Aggiunto import Properties

--- src/main/java/it/uniba/app/utility/Input.java
+++ src/main/java/it/uniba/app/utility/Input.java
@@ -3,6 +3,7 @@ package it.uniba.app.utility;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.util.Properties;
 
 /**
  * La classe gestisce l'input da parte dell'utente.
