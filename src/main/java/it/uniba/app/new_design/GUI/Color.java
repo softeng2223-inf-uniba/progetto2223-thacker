a19122647aff49a828a8f7185df257dec5e68687
shokk47 <gamerk789@gmail.com>
2023-06-05 11:16:17 +0200

    Aggiunto colore di reset

--- src/main/java/it/uniba/app/new_design/GUI/Color.java
+++ src/main/java/it/uniba/app/new_design/GUI/Color.java
@@ -3,7 +3,9 @@ import java.util.HashMap;
 import java.util.Map;
 class Color{
     private static final Map<String, String> COLOR = new HashMap<>(){
-
-    }
+        {
+            put("reset", "\\u001B[0m");
+        }
+    };
     private Color(){}
 }
