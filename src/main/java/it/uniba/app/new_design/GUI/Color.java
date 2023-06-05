a5219b5173d0eac6a84287800eaaf3bfef963a71
shokk47 <gamerk789@gmail.com>
2023-06-05 11:14:57 +0200

    Creato dizionario dei colori

--- src/main/java/it/uniba/app/new_design/GUI/Color.java
+++ src/main/java/it/uniba/app/new_design/GUI/Color.java
@@ -2,5 +2,7 @@ package it.uniba.app.new_design.GUI;
 import java.util.HashMap;
 import java.util.Map;
 class Color{
+    private static final Map<String, String> COLOR = new HashMap<>(){
 
+    }
 }
