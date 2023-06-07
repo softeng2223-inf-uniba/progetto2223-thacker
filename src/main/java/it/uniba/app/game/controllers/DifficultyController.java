fe9a1e26ceafdc8423a47fbd638fb640baa8167b
Vsb23 <v.birardi3@studenti.uniba.it>
2023-06-07 10:16:49 +0200

    Aggiunti attributi in DifficultyController

--- src/main/java/it/uniba/app/game/controllers/DifficultyController.java
+++ src/main/java/it/uniba/app/game/controllers/DifficultyController.java
@@ -2,4 +2,10 @@ package it.uniba.app.game.controllers;
 import it.uniba.app.game.entities.Difficulty;
 
 public class DifficultyController {
+    private static final String EASY_NAME = "Facile";
+    private static final String MEDIUM_NAME = "Medio";
+    private static final String HARD_NAME = "Difficile";
+    private static final int EASY_MAX_FAILED_ATTEMPTS = 50;
+    private static final int MEDIUM_MAX_FAILED_ATTEMPTS = 30;
+    private static final int HARD_MAX_FAILED_ATTEMPTS = 10;
 }
