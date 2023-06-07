1896805a47edb262a643f43fc86459d686e9e6e2
FredrickHZO <federico.armagno97@gmail.com>
2023-06-07 11:52:51 +0200

    Aggiunta base del metodo genShipMap

--- src/main/java/it/uniba/app/new_design/battleship/controller/GridController.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/GridController.java
@@ -9,4 +9,17 @@ public class GridController {
 
     private GridController() { }
 
+    /**
+     * Javadoc momentaneo.
+     * @return
+     */
+    public String genShipMap() {
+        String str = "      A    B    C    D    E    F    G    H    I    J\n\n";
+        for (int row = 0; row < Grid.getSize(); row++){
+            for (int col = 0; col < Grid.getSize(); col++) {
+                // genera stringa.
+            }
+        }
+        return str;
+    }
 }
