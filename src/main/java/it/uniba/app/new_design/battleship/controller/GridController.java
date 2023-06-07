9cefb5ab517adad3ca944c20c125155db1b17604
FredrickHZO <federico.armagno97@gmail.com>
2023-06-07 11:54:22 +0200

    Aggiunto import Coordinate in GridController

--- src/main/java/it/uniba/app/new_design/battleship/controller/GridController.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/GridController.java
@@ -1,6 +1,7 @@
 package it.uniba.app.new_design.battleship.controller;
 
 import it.uniba.app.new_design.battleship.entity.Grid;
+import it.uniba.app.utility.Coordinate;
 
 /**
  * Javadoc momentaneo.
@@ -16,7 +17,9 @@ public class GridController {
     public String genShipMap() {
         String str = "      A    B    C    D    E    F    G    H    I    J\n\n";
         for (int row = 0; row < Grid.getSize(); row++){
+            str += row + ":  ";
             for (int col = 0; col < Grid.getSize(); col++) {
+                Coordinate coords = new Coordinate(row, col);
                 // genera stringa.
             }
         }
