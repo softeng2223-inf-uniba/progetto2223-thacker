7f1dea56fac1bd17fc6142f2d865e4133a377947
FredrickHZO <federico.armagno97@gmail.com>
2023-06-07 11:51:00 +0200

    Aggiunto import Grid in GridController

--- src/main/java/it/uniba/app/new_design/battleship/controller/GridController.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/GridController.java
@@ -1,9 +1,12 @@
 package it.uniba.app.new_design.battleship.controller;
 
+import it.uniba.app.new_design.battleship.entity.Grid;
+
 /**
  * Javadoc momentaneo.
  */
 public class GridController {
 
     private GridController() { }
+
 }
