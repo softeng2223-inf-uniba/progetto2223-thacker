23db628ae3951d7c1e322294e6a6b8a86911f84b
FredrickHZO <federico.armagno97@gmail.com>
2023-06-07 11:33:20 +0200

    Aggiunti metodi di istanza e import necessari

--- src/main/java/it/uniba/app/new_design/battleship/entity/Grid.java
+++ src/main/java/it/uniba/app/new_design/battleship/entity/Grid.java
@@ -1,9 +1,15 @@
 package it.uniba.app.new_design.battleship.entity;
 
+import java.util.LinkedList;
+import it.uniba.app.utility.Coordinate;
+
 /**
  * Javadoc momentaneo.
  */
 public class Grid {
     private static final int SIZE = 10;
     private static final String STR_DOT = "\u00B7";
+
+    private Ship[][] map;
+    private LinkedList<Coordinate> hits;
 }
