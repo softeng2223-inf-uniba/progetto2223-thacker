3dc522dddd9c3a322fd6b4646a6306d0c683aa35
FredrickHZO <federico.armagno97@gmail.com>
2023-06-07 11:30:46 +0200

    Aggiunti attributi statici SIZE e STR_DOT

--- src/main/java/it/uniba/app/new_design/battleship/entity/Grid.java
+++ src/main/java/it/uniba/app/new_design/battleship/entity/Grid.java
@@ -4,5 +4,6 @@ package it.uniba.app.new_design.battleship.entity;
  * Javadoc momentaneo.
  */
 public class Grid {
-
+    private static final int SIZE = 10;
+    private static final String STR_DOT = "\u00B7";
 }
