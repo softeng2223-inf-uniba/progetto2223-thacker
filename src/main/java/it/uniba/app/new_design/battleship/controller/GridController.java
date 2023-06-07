5960a8a380584c018bd5ff7b254d9556bef597f3
FredrickHZO <federico.armagno97@gmail.com>
2023-06-07 16:18:44 +0200

    Corretto bug in GridController

--- src/main/java/it/uniba/app/new_design/battleship/controller/GridController.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/GridController.java
@@ -15,13 +15,12 @@ public class GridController {
 
     /**
      * Restituisce una oggetto di tipi {@code String} che
-     * contiene la mappa dei colpi che viene mostrata col
-     * quando comincia il gioco. Rappresenta la mappa del
-     * giocatore dove vengono segnati i colpi.
+     * contiene la mappa delle navi che viene mostrata col
+     * comando {@code /svelagriglia}.
      * @param grid oggetto che contiene la mappa delle navi.
      * @return stringa contenente tutta la mappa delle navi.
-     */
-    public static String genHitsMap(final Grid grid) {
+    */
+    public static String genShipMap(final Grid grid) {
         String str = "      A    B    C    D    E    F    G    H    I    J\n\n";
         for (int row = 0; row < Grid.getSize(); row++) {
             str += row + ":    ";
@@ -41,12 +40,13 @@ public class GridController {
 
     /**
      * Restituisce una oggetto di tipi {@code String} che
-     * contiene la mappa delle navi che viene mostrata col
-     * comando {@code /svelagriglia}.
+     * contiene la mappa dei colpi che viene mostrata col
+     * quando comincia il gioco. Rappresenta la mappa del
+     * giocatore dove vengono segnati i colpi.
      * @param grid oggetto che contiene la mappa delle navi.
      * @return stringa contenente tutta la mappa delle navi.
      */
-    public String genShipMap(final Grid grid) {
+    public String genHitspMap(final Grid grid) {
         String str = "      A    B    C    D    E    F    G    H    I    J\n\n";
         for (int row = 0; row < Grid.getSize(); row++){
             str += row + ":   ";
