b401ef8a8f3bbd956920d62292b5f1ff2514f796
FredrickHZO <federico.armagno97@gmail.com>
2023-06-07 14:01:15 +0200

    Aggiunto metodo genHitsMap

--- src/main/java/it/uniba/app/new_design/battleship/controller/GridController.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/GridController.java
@@ -3,6 +3,7 @@ package it.uniba.app.new_design.battleship.controller;
 import it.uniba.app.new_design.battleship.entity.Grid;
 import it.uniba.app.utility.Coordinate;
 import it.uniba.app.utility.Color;
+import it.uniba.app.new_design.battleship.entity.Ship;
 
 /**
  * Javadoc momentaneo.
@@ -10,6 +11,29 @@ import it.uniba.app.utility.Color;
 public class GridController {
 
     private GridController() { }
+
+    /**
+     * Javadoc momentaneo.
+     * @param grid
+     * @return
+     */
+    public static String genHitsMap(final Grid grid) {
+        String str = "      A    B    C    D    E    F    G    H    I    J\n\n";
+        for (int row = 0; row < Grid.getSize(); row++) {
+            str += row + ":    ";
+            for (int col = 0; col < Grid.getSize(); col++) {
+                Coordinate coords = new Coordinate(row, col);
+                Ship item = grid.get(coords);
+                if (item == null) {
+                    str += Grid.getWaterSymbol() + "    ";
+                } else {
+                    str += item + "    ";
+                }
+            }
+            str += "\n\n";
+        }
+        return str;
+    }
 
     /**
      * Javadoc momentaneo.
