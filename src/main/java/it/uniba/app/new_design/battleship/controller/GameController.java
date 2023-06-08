40c50a8658e914ea939ed69dfc7f49102a54c6ca
anto31ad <anto31.ad@gmail.com>
2023-06-08 11:19:28 +0200

    Aggiunta difficoltà in GameController

--- src/main/java/it/uniba/app/new_design/battleship/controller/GameController.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/GameController.java
@@ -1,10 +1,14 @@
 package it.uniba.app.new_design.battleship.controller;
 
+import it.uniba.app.game.controllers.DifficultyController;
+import it.uniba.app.game.entities.Difficulty;
 import it.uniba.app.game.exceptions.SessionAlreadyStartedException;
 import it.uniba.app.game.exceptions.SessionNotStartedException;
 
 public class GameController {
     private static boolean isSessionStarted = false;
+
+    private static Difficulty difficulty = new Difficulty();
 
     private GameController() { }
 
@@ -12,6 +16,8 @@ public class GameController {
         if (isSessionStarted) {
             throw new SessionAlreadyStartedException();
         }
+
+        DifficultyController.setEasy(difficulty);
 
         isSessionStarted = true;
     }
