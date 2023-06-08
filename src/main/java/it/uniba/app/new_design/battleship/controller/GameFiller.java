d74d44948a6e594db40da4fb329191fedec00ad2
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-06-08 09:27:51 +0200

    Aggiunta costante RAND

--- src/main/java/it/uniba/app/new_design/battleship/controller/GameFiller.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/GameFiller.java
@@ -4,6 +4,7 @@ package it.uniba.app.new_design.battleship.controller;
  * Javadoc momentaneo.
  */
 public final class GameFiller {
+    private static final Random RAND = new Random();
 
     private GameFiller() { }
 
