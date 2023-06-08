0bc362901cb74c0a279253a1c8196346ae63c100
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-06-08 09:28:32 +0200

    Aggiunte costanti POSSIBLE_DIRECTIONS e VERTICAL

--- src/main/java/it/uniba/app/new_design/battleship/controller/GameFiller.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/GameFiller.java
@@ -5,6 +5,8 @@ package it.uniba.app.new_design.battleship.controller;
  */
 public final class GameFiller {
     private static final Random RAND = new Random();
+    private static final int POSSIBLE_DIRECTIONS = 2;
+    private static final int VERTICAL = 0;
 
     private GameFiller() { }
 
