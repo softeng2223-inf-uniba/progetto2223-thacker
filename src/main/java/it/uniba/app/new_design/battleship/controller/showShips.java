32a52ee169f32e72bd9b80ac2d648547588ff7ba
Alessandro Carli <a.carli3@studenti.uniba.it>
2023-06-08 11:36:35 +0200

    Aggiunto JavDoc classe ShowShips

--- src/main/java/it/uniba/app/new_design/battleship/controller/showShips.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/showShips.java
@@ -2,6 +2,15 @@ package it.uniba.app.new_design.battleship.controller;
 
 import it.uniba.app.new_design.battleship.entity.Ship;
 
+/**
+ * Classe necessaria per eseguire il comando
+ * {@code /mostranavi} e ottenere l'output desiderato.
+ * Permette di stampare a video, di ogni tipo
+ * di nave, il nome, la dimensione in quadrati e il numero
+ * di esemplari da affondare. Utilizza i metodi
+ * implementati in ciascuna sottoclasse di {@code Ship}
+ * relativi alla stampa delle informazioni di ciascuna nave.
+*/
 public final class showShips {
 
     private showShips() { }
