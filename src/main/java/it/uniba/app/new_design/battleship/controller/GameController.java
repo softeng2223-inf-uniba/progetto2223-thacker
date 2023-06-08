fd5127cb4317cf69801597675376968e3a8c99d6
anto31ad <anto31.ad@gmail.com>
2023-06-08 15:17:19 +0200

    Aggiornata GameController sfruttando Game (entity)

--- src/main/java/it/uniba/app/new_design/battleship/controller/GameController.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/GameController.java
@@ -1,11 +1,9 @@
 package it.uniba.app.new_design.battleship.controller;
 
-import java.util.LinkedList;
-
 import it.uniba.app.game.controllers.DifficultyController;
 import it.uniba.app.game.entities.Difficulty;
+import it.uniba.app.new_design.battleship.entity.Game;
 import it.uniba.app.new_design.battleship.entity.Grid;
-import it.uniba.app.new_design.battleship.entity.Ship;
 import it.uniba.app.game.exceptions.DifficultyNotSetException;
 import it.uniba.app.game.exceptions.SessionAlreadyStartedException;
 import it.uniba.app.game.exceptions.SessionNotStartedException;
@@ -18,14 +16,7 @@ import it.uniba.app.game.exceptions.SessionNotStartedException;
  * TODO espandi.
  */
 public final class GameController {
-    private static final LinkedList<Ship> SHIPS = getShipSet();
-
-    private static boolean isSessionStarted = false;
-    private static boolean isDifficultySet = false;
-
-    private static Difficulty difficulty = new Difficulty();
-    private static Grid grid;
-
+    private static Game game = new Game();
 
     private GameController() { }
 
@@ -40,18 +31,7 @@ public final class GameController {
      */
     public static void startSession()
         throws SessionAlreadyStartedException, DifficultyNotSetException {
-            if (isSessionStarted) {
-                throw new SessionAlreadyStartedException();
-            }
-
-            if (!isDifficultySet) {
-                throw new DifficultyNotSetException();
-            }
-
-            grid = new Grid();
-            GameFiller.randomlyFill(SHIPS, grid);
-
-            isSessionStarted = true;
+            game.startSession();
         }
 
     /**
@@ -60,46 +40,40 @@ public final class GameController {
      * @throws SessionNotStartedException Non è possibile terminare una sessione se non è in corso.
      */
     public static void endSession() throws SessionNotStartedException {
-        if (!isSessionStarted) {
-            throw new SessionNotStartedException();
-        }
-        isSessionStarted = false;
+        game.endSession();
     }
 
     /**
      * Imposta la difficoltà ad Easy.
      */
     public static void setEasyDifficulty() throws SessionAlreadyStartedException {
-        if (isSessionStarted) {
-            throw new SessionAlreadyStartedException();
-        }
-
-        DifficultyController.setEasy(difficulty);
-        isDifficultySet = true;
+        Difficulty diff = new Difficulty();
+        DifficultyController.setEasy(diff);
+        try {
+            game.setDifficulty(diff);
+        } catch (CloneNotSupportedException e) { }
     }
 
     /**
      * Imposta la difficoltà a Medium.
      */
     public static void setMediumDifficulty() throws SessionAlreadyStartedException {
-        if (isSessionStarted) {
-            throw new SessionAlreadyStartedException();
-        }
-
-        DifficultyController.setMedium(difficulty);
-        isDifficultySet = true;
+        Difficulty diff = new Difficulty();
+        DifficultyController.setMedium(diff);
+        try {
+            game.setDifficulty(diff);
+        } catch (CloneNotSupportedException e) { }
     }
 
     /**
      * Imposta la difficoltà a Hard.
      */
     public static void setHardDifficulty() throws SessionAlreadyStartedException {
-        if (isSessionStarted) {
-            throw new SessionAlreadyStartedException();
-        }
-
-        DifficultyController.setHard(difficulty);
-        isDifficultySet = true;
+        Difficulty diff = new Difficulty();
+        DifficultyController.setHard(diff);
+        try {
+            game.setDifficulty(diff);
+        } catch (CloneNotSupportedException e) { }
     }
 
     /**
@@ -109,7 +83,7 @@ public final class GameController {
      * @throws CloneNotSupportedException
      */
     static Difficulty getDifficulty() throws CloneNotSupportedException {
-        return difficulty.clone();
+        return game.getDifficulty();
     }
 
     /**
@@ -123,31 +97,7 @@ public final class GameController {
      * @throws SessionNotStartedException
      */
     static Grid getSessionGrid() throws SessionNotStartedException {
-        if (!isSessionStarted) {
-            throw new SessionNotStartedException();
-        }
-        return grid.clone();
-    }
-
-
-    /* === PRIVATE METHODS === */
-
-      /**
-     * Restituisce una lista di navi contenente, per ogni tipo di nave, tante istanze
-     * quanti sono numero di esemplari previsti per tale tipo.
-     *
-     * @return lista di navi
-     */
-    //TODO improve
-    private static LinkedList<Ship> getShipSet() {
-        LinkedList<Ship> shipsList = new LinkedList<Ship>();
-
-        for (int tId = 0; tId < Ship.getNumberOfTypes(); tId++) {
-            for (int i = 0; i < Ship.getMaxInstances(tId); i++) {
-                shipsList.add(new Ship(tId));
-            }
-        }
-        return shipsList;
+        return game.getSessionGrid();
     }
 
 }
