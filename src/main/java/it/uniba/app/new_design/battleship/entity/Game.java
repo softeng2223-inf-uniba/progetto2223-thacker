528741e0060c80eac26f56fdfe1b99414343ddba
anto31ad <anto31.ad@gmail.com>
2023-06-08 18:47:25 +0200

    Rimossa gestione di GameExceptions da Game

--- src/main/java/it/uniba/app/new_design/battleship/entity/Game.java
+++ src/main/java/it/uniba/app/new_design/battleship/entity/Game.java
@@ -5,10 +5,6 @@ import java.util.LinkedList;
 import it.uniba.app.game.entities.Difficulty;
 import it.uniba.app.new_design.battleship.controller.GameFiller;
 
-import it.uniba.app.game.exceptions.DifficultyNotSetException;
-import it.uniba.app.game.exceptions.SessionAlreadyStartedException;
-import it.uniba.app.game.exceptions.SessionNotStartedException;
-
 /**
  * Entity class <hr>
  *
@@ -16,9 +12,6 @@ import it.uniba.app.game.exceptions.SessionNotStartedException;
  */
 public final class Game {
     private static final LinkedList<Ship> SHIPS = getShipSet();
-
-    private boolean isSessionStarted;
-    private boolean isDifficultySet;
 
     private Difficulty difficulty;
     private Grid grid;
@@ -30,49 +23,20 @@ public final class Game {
     /** Istanzia un oggetto della classe Game. */
     public Game() {
         difficulty = new Difficulty();
-        isSessionStarted = false;
-        isDifficultySet = false;
     }
 
     /**
      * Avvia una nuova sessione di gioco.<hr>
      * Esegue tutte le inizializzazioni necessarie per giocare.
-     *
-     * @throws SessionAlreadyStartedException
-     *      Non è possibile avviare un altra sessione se un'altra è in corso.
-     * @throws DifficultyNotSetException
-     *      Non è possibile avviare una sessione se la difficoltà non è stata impostata.
      */
-    public void startSession()
-        throws SessionAlreadyStartedException, DifficultyNotSetException {
-            if (isSessionStarted) {
-                throw new SessionAlreadyStartedException();
-            }
-
-            if (!isDifficultySet) {
-                throw new DifficultyNotSetException();
-            }
-
+    public void startSession() {
             grid = new Grid();
             GameFiller.randomlyFill(SHIPS, grid);   //TODO cercare di rimuovere questa dipendenza
 
             totAttempts = 0;
             failedAttempts = 0;
-
-            isSessionStarted = true;
         }
 
-    /**
-     * Termina una sessione di gioco in corso.
-     *
-     * @throws SessionNotStartedException Non è possibile terminare una sessione se non è in corso.
-     */
-    public void endSession() throws SessionNotStartedException {
-        if (!isSessionStarted) {
-            throw new SessionNotStartedException();
-        }
-        isSessionStarted = false;
-    }
 
     /**
      * Imposta una difficoltà.
@@ -80,13 +44,8 @@ public final class Game {
      * @param choice difficoltà da impostare
      */
     public void setDifficulty(final Difficulty choice)
-        throws SessionAlreadyStartedException, CloneNotSupportedException {
-            if (isSessionStarted) {
-                throw new SessionAlreadyStartedException();
-            }
-
+        throws CloneNotSupportedException {
             difficulty = choice.clone();
-            isDifficultySet = true;
         }
 
     /**
@@ -107,12 +66,8 @@ public final class Game {
      * operare sulla copia non influenza il corso della sessione.
      *
      * @return griglia della sessione corrente nell'istante corrente
-     * @throws SessionNotStartedException
      */
-    public Grid getSessionGrid() throws SessionNotStartedException {
-        if (!isSessionStarted) {
-            throw new SessionNotStartedException();
-        }
+    public Grid getSessionGrid() {
         return grid.clone();
     }
 
@@ -122,39 +77,17 @@ public final class Game {
      * durante una sessione di gioco.
      * 
      * @return tentativi totali nell'istante corrente
-     * @throws SessionNotStartedException
      */
-    public int getAttempts() throws SessionNotStartedException {
-        if(!isSessionStarted) {
-            throw new SessionNotStartedException();
-        }
+    public int getAttempts() {
         return totAttempts;
     }
-
-    /**
-     * Incrementa numero di tentativi corrente durante una sessione di gioco.
-     * 
-     * @throws SessionNotStartedException
-     */
-    // TODO decidere visibilità
-    void increaseAttempts() throws SessionNotStartedException {
-        if(!isSessionStarted) {
-            throw new SessionNotStartedException();
-        }
-        totAttempts += 1;
-    }
-
 
     /**
      * Fornisce il numero di tentativi falliti fino ad un certo istante di una sessione di gioco.
      * 
      * @return tentativi falliti all'istante corrente
-     * @throws SessionNotStartedException
      */
-    public int getFailedAttempts() throws SessionNotStartedException {
-        if(!isSessionStarted) {
-            throw new SessionNotStartedException();
-        }
+    public int getFailedAttempts() {
         return failedAttempts;
     }
 
