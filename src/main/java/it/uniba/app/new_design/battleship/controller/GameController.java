2c8cef3c9219a62713598deb2ce9e22752833ad8
anto31ad <anto31.ad@gmail.com>
2023-06-08 11:04:13 +0200

    Aggiunto startSession() per iniziare una partita

--- src/main/java/it/uniba/app/new_design/battleship/controller/GameController.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/GameController.java
@@ -1,5 +1,15 @@
 package it.uniba.app.new_design.battleship.controller;
 
+import it.uniba.app.game.exceptions.SessionAlreadyStartedException;
+
 public class GameController {
-    
+    private static boolean isSessionStarted = false;
+
+    public static void startSession() throws SessionAlreadyStartedException {
+        if (isSessionStarted) {
+            throw new SessionAlreadyStartedException();
+        }
+
+        isSessionStarted = true;
+    }
 }
