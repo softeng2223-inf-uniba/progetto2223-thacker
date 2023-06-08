aacc135d283dcbf96d7af71d629d1e28e17c4794
Vsb23 <v.birardi3@studenti.uniba.it>
2023-06-08 10:06:36 +0200

    Inseriti attributi iniziali

--- src/main/java/it/uniba/app/new_design/battleship/controller/HelpController.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/HelpController.java
@@ -1,4 +1,18 @@
 package it.uniba.app.new_design.battleship.controller;
 public final class HelpController{
+    private static final int DELAY = 10;
+    private static final String TEXT =
+            "Benvenuto in BattleShip!\n\n"
+                    + "Scopo del gioco: affondare tutte le navi prima "
+                    + "di aver terminato il numero di tentativi fallibili.\n"
+                    + "Per giocare, utilizzare i seguenti comandi:\n\n"
+                    + "/gioca \n"
+                    + "/facile \n"
+                    + "/medio \n"
+                    + "/difficile \n"
+                    + "/mostralivello \n"
+                    + "/svelagriglia \n"
+                    + "/mostranavi \n"
+                    + "/esci";
     private HelpController(){}
 }
