721cfd8d5e8349c5ed885a097c53440569e75fb1
shokk47 <gamerk789@gmail.com>
2023-06-09 23:06:55 +0200

    Creato GameControllerTest.java

---
+++ src/test/java/it/uniba/app/battleship/controller/GameControllerTest.java
@@ -0,0 +1,11 @@
+package it.uniba.app.battleship.controller;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
+/**
+ *  TODO
+ */
+class GameControllerTest {
+
+}
