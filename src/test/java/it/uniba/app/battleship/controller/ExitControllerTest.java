61f5508acdd668c1ef018312de8ca29e47136972
shokk47 <gamerk789@gmail.com>
2023-06-09 22:14:29 +0200

    Corretto errori vari
    Corretto errori checkstyle e PMD

--- src/test/java/it/uniba/app/battleship/controller/ExitControllerTest.java
+++ src/test/java/it/uniba/app/battleship/controller/ExitControllerTest.java
@@ -2,9 +2,16 @@ package it.uniba.app.battleship.controller;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
+/**
+ *  Test suite per la classe ExitController
+ */
 public class ExitControllerTest {
+    /**
+     *   Verifica il corretto funzionamento del
+     *   metodo requestExit() della classe ExitController.
+     */
     @Test
-    public void testRequestExit() {
+    void testRequestExit() {
         ExitController.requestExit();
         assertTrue(ExitController.isExitRequested());
     }
