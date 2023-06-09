2fa8588d52d5b756cad82713238fabe2e6aba7e5
anto31ad <anto31.ad@gmail.com>
2023-06-09 11:51:36 +0200

    Aggiunto handlePlay() in CommandHandler

--- src/main/java/it/uniba/app/new_design/battleship/controller/CommandHandler.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/CommandHandler.java
@@ -3,6 +3,8 @@ package it.uniba.app.new_design.battleship.controller;
 
 import java.io.IOException;
 
+import it.uniba.app.game.exceptions.DifficultyNotSetException;
+import it.uniba.app.game.exceptions.SessionAlreadyStartedException;
 import it.uniba.app.new_design.battleship.entity.Game;
 import it.uniba.app.utility.Input;
 
@@ -30,4 +32,16 @@ public final class CommandHandler {
     private void handleHelp() {
         HelpController.showHelp();
     }
+
+    private void handlePlay(Game game) {
+        try {
+            GameController.setEasyDifficulty(game);
+            GameController.startSession(game);
+        } catch (DifficultyNotSetException e ) {
+            System.out.println(e.getMessage());
+        } catch (SessionAlreadyStartedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
