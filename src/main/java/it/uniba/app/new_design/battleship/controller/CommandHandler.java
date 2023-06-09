292ec292ea4f9f68f8faee76de8b7ae02948f1ee
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-06-09 11:25:54 +0200

    Aggiunto metodo execute a CH

--- src/main/java/it/uniba/app/new_design/battleship/controller/CommandHandler.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/CommandHandler.java
@@ -1,7 +1,19 @@
 package it.uniba.app.new_design.battleship.controller;
 
 public final class CommandHandler {
-    
+
     private CommandHandler() { }
 
+    public void execute() {
+        try {
+            String command = Input.get().toLowerCase();
+            switch (command) {
+                case "/help"            -> handleHelp();
+                case "/mostranavi"      -> handleShowShip();
+                default                 -> System.err.println("[CH] Comando inesistente.");
+            }
+        } catch (IOException e) {
+            System.out.println("Si è verificato un errore durante la lettura del comando: " + e.getMessage());
+        }
+    }
 }
