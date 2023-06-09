af6f895a6e797df64b0938d3b937848b6696e7ce
shokk47 <gamerk789@gmail.com>
2023-06-09 22:05:48 +0200

    Aggiunto test : testRequestExit()

--- src/test/java/it/uniba/app/battleship/controller/ExitControllerTest.java
+++ src/test/java/it/uniba/app/battleship/controller/ExitControllerTest.java
@@ -1,5 +1,11 @@
 package it.uniba.app.battleship.controller;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 public class ExitControllerTest {
+    @Test
+    public void testRequestExit() {
+        ExitController.requestExit();
+        assertTrue(ExitController.isExitRequested());
+    }
 }
