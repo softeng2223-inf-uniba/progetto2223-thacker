ddbe806c8a6f90904c22b36d9e90652de1030750
shokk47 <gamerk789@gmail.com>
2023-06-09 22:10:50 +0200

    Corretto import

--- src/test/java/it/uniba/app/battleship/controller/ExitControllerTest.java
+++ src/test/java/it/uniba/app/battleship/controller/ExitControllerTest.java
@@ -1,7 +1,7 @@
 package it.uniba.app.battleship.controller;
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+
 public class ExitControllerTest {
     @Test
     public void testRequestExit() {
