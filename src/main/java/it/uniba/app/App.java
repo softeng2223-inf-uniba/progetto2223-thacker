a7665e41dce1b7af71e47d81ec1d446862cccb30
Giuseppe <g.durso15@studenti.uniba.it>
2023-06-09 18:30:21 +0200

    Eliminata temporaneamente gestione di FlagHandler

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -3,8 +3,6 @@ package it.uniba.app;
 import it.uniba.app.battleship.entity.Game;
 import it.uniba.app.commandline.controller.CommandHandler;
 import it.uniba.app.battleship.controller.ExitController;
-import it.uniba.app.utility.commands.FlagHandler;
-import it.uniba.app.battleship.entity.Game;
 
 /**
  * Main class of the application.
@@ -28,9 +26,11 @@ public final class App {
     public static void main(final String[] args) {
         System.out.println(new App().getGreeting());
 
+        /* TODO reimplementare FlagHandler
         if (args.length > 0) {
             FlagHandler.execute(args);
         }
+        */
 
         Game game = new Game();
         System.out.println("Digita un comando");
