0e63f89721dbcc071889bb654689fd48963f4004
anto31ad <anto31.ad@gmail.com>
2023-06-09 08:43:53 +0200

    Aggiornato javadoc in ExitController

--- src/main/java/it/uniba/app/new_design/battleship/controller/ExitController.java
+++ src/main/java/it/uniba/app/new_design/battleship/controller/ExitController.java
@@ -2,6 +2,8 @@ package it.uniba.app.new_design.battleship.controller;
 
 /**
  * Gestisce la richiesta di uscita dall'applicazione.
+ * <hr>
+ * <b>Control class</b>
  */
 public final class ExitController {
     private static boolean requestedExit = false;
@@ -16,7 +18,7 @@ public final class ExitController {
     }
 
     /**
-     * Fornisce lo stato della richiesta.
+     * Fornisce lo stato della richiesta di uscita dalla applicazione.
      *
      * @return <code>true</code> se è stata effettuata una richiesta, <code>false</code> altrimenti.
      */
