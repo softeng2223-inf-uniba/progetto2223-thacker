df32af85390bfc73d5da31bd1496ab2606d55fe0
shokk47 <gamerk789@gmail.com>
2023-06-10 17:37:09 +0200

    Aggiunti casi di test

--- src/test/java/it/uniba/app/battleship/controller/ExitControllerTest.java
+++ src/test/java/it/uniba/app/battleship/controller/ExitControllerTest.java
@@ -1,18 +1,64 @@
 package it.uniba.app.battleship.controller;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *  Test suite per la classe ExitController.
  */
 class ExitControllerTest {
+    ExitController instance;
+    @BeforeEach
+    void setUp() throws Exception {
+        Method getInstanceMethod = ExitController.class.getDeclaredMethod("getInstance");
+        getInstanceMethod.setAccessible(true);
+        instance = (ExitController) getInstanceMethod.invoke(null);
+    }
     /**
-     *   Verifica il corretto funzionamento del
-     *   metodo requestExit() della classe ExitController.
+     *  Verifica che chiamare getInstance() più volte restituisca
+     *  sempre la stessa instance di ExitController.
      */
     @Test
-    void testRequestExit() {
-        ExitController.requestExit();
-        assertTrue(ExitController.isExitRequested(), "ExitController.requestExit() non funziona correttamente");
+    void testGetInstance_ReturnsSameInstance(){
+        assertEquals(instance, ExitController.getInstance());
+    }
+    /**
+     *  Verifica che chiamare getInstance() restituisca un'istanza
+     *  di ExitController non nulla.
+     */
+    @Test
+    void testGetInstance_ReturnsNotNullInstance(){
+        assertTrue(instance != null);
+    }
+    /**
+     *  Verifica che chiamare il metodo requestExit() imposti
+     *  correttamente l'attributo requestExit a true.
+     */
+    @Test
+    void testRequestExit(){
+        instance.requestExit();
+        assertTrue(instance.isExitRequested());
+    }
+    /**
+     * Verifica che chiamare il metodo requestExit() due volte
+     * consecutivamente imposti comunque l'attributo requestExit a true.
+     */
+    @Test
+    void testRequestExit_twice(){
+        instance.requestExit();
+        instance.requestExit();
+        assertTrue(instance.isExitRequested());
+    }
+    /**
+     *  Verifica che chiamare il metodo isExitRequested() senza
+     *  una richiesta attiva restituisca false.
+     */
+    @Test
+    void testIsExitRequested_NotRequested(){
+        assertFalse(instance.isExitRequested());
     }
 }
