f771e1f4fb5d3a9c4681f12742221a5953a3886e
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-06-11 22:42:54 +0200

    Aggiunti attributi alla classe Time

--- src/main/java/it/uniba/app/battleship/entity/Time.java
+++ src/main/java/it/uniba/app/battleship/entity/Time.java
@@ -1,5 +1,9 @@
 package it.uniba.app.battleship.entity;
 
 public class Time {
-    
+
+    private static long timeLimitMinute;
+    private static long startTimeMill;
+    private static long timeLimitMilliseconds;
+    private static long currentTimeMill;
 }
