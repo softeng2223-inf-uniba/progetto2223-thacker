9700bb14b8a251846201eed3a8ad205e1262e456
FredrickHZO <federico.armagno97@gmail.com>
2023-06-11 10:38:34 +0200

    Eliminato metodo inutilizzato

--- src/main/java/it/uniba/app/battleship/controller/GridController.java
+++ src/main/java/it/uniba/app/battleship/controller/GridController.java
@@ -6,7 +6,6 @@ import java.util.Random;
 import it.uniba.app.battleship.entity.Coordinate;
 import it.uniba.app.battleship.entity.Grid;
 import it.uniba.app.battleship.entity.Ship;
-import it.uniba.app.utility.Color;
 
 /**
  * Classe che contiene i metodi utili alla visualizzazione della
@@ -48,37 +47,6 @@ public final class GridController {
                 }
             }
             str += "\n\n";
-        }
-        return str;
-    }
-
-    /**
-     * Restituisce una oggetto di tipi {@code String} che
-     * contiene la mappa dei colpi che viene mostrata col
-     * quando comincia il gioco. Rappresenta la mappa del
-     * giocatore dove vengono segnati i colpi.
-     * @param grid oggetto che contiene la mappa delle navi.
-     * @return stringa contenente tutta la mappa delle navi.
-     */
-    public static String genHitsMap(final Grid grid) {
-        String str = "      A    B    C    D    E    F    G    H    I    J\n\n";
-        for (int row = 0; row < Grid.getSize(); row++) {
-            str += row + ":    ";
-            for (int col = 0; col < Grid.getSize(); col++) {
-                Coordinate coords = new Coordinate(row, col);
-                if (grid.isCellHit(coords)) {
-                    if (grid.isCellEmpty(coords)) {
-                        str += Color.get("blue") + Grid.getWaterSymbol()
-                            + Color.getReset() + "    ";
-                    } else {
-                        str += Color.get("red") + Grid.getHitSymbol()
-                         + Color.getReset() + "    ";
-                    }
-                } else {
-                    str += Grid.getDotSymbol() + Grid.getHitSymbol()
-                        + Color.getReset() + "    ";
-                }
-            }
         }
         return str;
     }
