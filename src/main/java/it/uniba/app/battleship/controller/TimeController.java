2198a72a5e41a0fd3a55c8c6024c68dece20b14f
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-06-11 22:58:22 +0200

    Aggiunte costanti e costruttore a TimeController

--- src/main/java/it/uniba/app/battleship/controller/TimeController.java
+++ src/main/java/it/uniba/app/battleship/controller/TimeController.java
@@ -1,5 +1,10 @@
 package it.uniba.app.battleship.controller;
 
 public final class TimeController {
-    
+
+    private static final int SECOND = 60;
+    private static final int MILLISECONDS = 1000;
+
+    private TimeController() { };
+
 }
