51193b4c3c5fc2275db79892bcc51e6244458e1f
Annarita Bruno <127734844+annabr98@users.noreply.github.com>
2023-06-11 23:02:43 +0200

    Aggiunto metodo setTimeLimit

--- src/main/java/it/uniba/app/battleship/controller/TimeController.java
+++ src/main/java/it/uniba/app/battleship/controller/TimeController.java
@@ -1,5 +1,8 @@
 package it.uniba.app.battleship.controller;
 
+import it.uniba.app.battleship.entity.Game;
+import it.uniba.app.battleship.exception.SessionAlreadyStartedException;
+
 public final class TimeController {
 
     private static final int SECOND = 60;
@@ -10,5 +13,13 @@ public final class TimeController {
     public static void setTime(final Time time) {
         time.setTimeLimitMill(time.getTimeLimitMin() * SECOND * MILLISECONDS);
         time.setStartTimeMill(System.currentTimeMillis());
+    }
+
+    public static void setTimeLimit(final Game game, final long timeSet)
+    throws SessionAlreadyStartedException {
+        if (game.isSessionStarted()) {
+            throw new SessionAlreadyStartedException();
+        }
+        game.getTime().setTimeLimitMin(timeSet);
     }
 }
