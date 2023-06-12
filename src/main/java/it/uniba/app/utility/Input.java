3b28c8a407f6d790503471a41c20530180cb114f
shokk47 <gamerk789@gmail.com>
2023-06-12 22:57:42 +0200

    Corretti errori di checkstyle in Input

--- src/main/java/it/uniba/app/utility/Input.java
+++ src/main/java/it/uniba/app/utility/Input.java
@@ -3,8 +3,6 @@ package it.uniba.app.utility;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.Properties;
-import java.io.FileInputStream;
 
 /**
  * La classe gestisce l'input da parte dell'utente.
