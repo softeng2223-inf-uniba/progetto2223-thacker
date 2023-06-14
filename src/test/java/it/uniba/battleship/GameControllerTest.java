7999b11512cd93ce38240f2086c55c04bd46e330
shokk47 <gamerk789@gmail.com>
2023-06-14 20:21:52 +0200

    Aggiunti attributi

--- src/test/java/it/uniba/battleship/GameControllerTest.java
+++ src/test/java/it/uniba/battleship/GameControllerTest.java
@@ -1,5 +1,8 @@
 package it.uniba.battleship;
 
+import it.uniba.app.battleship.GameController;
+import it.uniba.battleship.entity.GameMock;
+
 import it.uniba.app.battleship.entity.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,4 +15,6 @@ import it.uniba.app.battleship.exception.OutOfMapException;
 import it.uniba.app.battleship.exception.SessionAlreadyStartedException;
 import it.uniba.app.battleship.exception.SessionNotStartedException;
 public class GameControllerTest {
+    private GameController gc;
+    private GameMock gameMock;
 }
