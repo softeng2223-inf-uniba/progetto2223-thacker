e5fe8fbeedd533d14f6444fefc0857b46f52bc39
Giuseppe <g.durso15@studenti.uniba.it>
2023-06-14 22:04:06 +0200

    Aggiunto metodo setUp

--- src/test/java/it/uniba/app/commandline/FlahHandlerTest.java
+++ src/test/java/it/uniba/app/commandline/FlahHandlerTest.java
@@ -1,8 +1,15 @@
 package it.uniba.app.commandline;
 
+import org.junit.jupiter.api.BeforeEach;
+
 /**
  * javadoc momentaneo.
  */
 class FlahHandlerTest {
+    private FlagHandler fh;
 
+    @BeforeEach
+    void setUp() {
+        fh = FlagHandler.getInstance();
+    }
 }
