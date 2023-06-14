b9dea9971797cbb6bbe222b121f06bc03df3efcb
shokk47 <gamerk789@gmail.com>
2023-06-14 20:13:02 +0200

    Aggiunti attributi privati di GameMock

--- src/test/java/it/uniba/battleship/entity/GameMock.java
+++ src/test/java/it/uniba/battleship/entity/GameMock.java
@@ -1,4 +1,22 @@
 package it.uniba.battleship.entity;
 
+import it.uniba.app.battleship.entity.Coordinate;
+import it.uniba.app.battleship.entity.Difficulty;
+import it.uniba.app.battleship.entity.Grid;
+import it.uniba.app.battleship.entity.Time;
+
+import java.util.HashSet;
+
 public class GameMock {
+    private boolean sessionStartedMock;
+    private boolean diffSetMock;
+    private int sunkShipsMock;
+    private Difficulty difficultyMock;
+    private Grid gridMock;
+    private Time timeMock;
+    private HashSet<Coordinate> attemptsMock;
+    private int failedAttemptsMock;
+    public GameMock() {
+
+    }
 }
